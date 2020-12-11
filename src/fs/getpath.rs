8993e1b5cdf2a49526f2e4c6d26919d27979bff9
Dan Gohman <dev@sunfishcode.online>
2020-12-11 11:15:02 -0800

    Whitespace cleanup.

--- src/fs/getpath.rs
+++ src/fs/getpath.rs
@@ -12,7 +12,7 @@ pub fn getpath<Fd: AsRawFd>(fd: &Fd, buf: &mut [u8]) -> io::Result<()> {
 
 unsafe fn _getpath(fd: RawFd, buf: &mut [u8]) -> io::Result<()> {
     // From the macOS `fcntl` man page:
-    // `F_GETPATH` - Get the path of the file descriptor `Fildes`.  The argument
+    // `F_GETPATH` - Get the path of the file descriptor `Fildes`. The argument
     //               must be a buffer of size `MAXPATHLEN` or greater.
     //
     // https://developer.apple.com/library/archive/documentation/System/Conceptual/ManPages_iPhoneOS/man2/fcntl.2.html
