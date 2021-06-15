cfallin/posish

Port to io-lifetimes.

src/fs/copy_file_range.rs
@@ -1,25 +1,26 @@
 use crate::negone_err;
+use io_experiment::{AsFd, BorrowedFd};
 use std::{convert::TryInto, io, mem, ptr};
-use unsafe_io::{os::posish::AsRawFd, AsUnsafeHandle, UnsafeHandle};
+use unsafe_io::os::posish::AsRawFd;
 
 /// `copy_file_range(fd_in, off_in, fd_out, off_out, len, 0)`
 #[inline]
-pub fn copy_file_range<InUnsafeHandle: AsUnsafeHandle, OutUnsafeHandle: AsUnsafeHandle>(
-    fd_in: &InUnsafeHandle,
+pub fn copy_file_range<InFd: AsFd, OutFd: AsFd>(
+    fd_in: &InFd,
     off_in: Option<&mut u64>,
-    fd_out: &OutUnsafeHandle,
+    fd_out: &OutFd,
     off_out: Option<&mut u64>,
     len: u64,
 ) -> io::Result<u64> {
-    let fd_in = fd_in.as_unsafe_handle();
-    let fd_out = fd_out.as_unsafe_handle();
+    let fd_in = fd_in.as_fd();
+    let fd_out = fd_out.as_fd();
     unsafe { _copy_file_range(fd_in, off_in, fd_out, off_out, len) }
 }
 
 unsafe fn _copy_file_range(
-    fd_in: UnsafeHandle,
+    fd_in: BorrowedFd<'_>,
     off_in: Option<&mut u64>,
-    fd_out: UnsafeHandle,
+    fd_out: BorrowedFd<'_>,
     off_out: Option<&mut u64>,
     len: u64,
 ) -> io::Result<u64> {

src/fs/dir.rs
@@ -4,6 +4,7 @@
 use crate::fs::android::{seekdir as libc_seekdir, telldir as libc_telldir};
 use crate::fs::FileType;
 use errno::{set_errno, Errno};
+use io_experiment::{IntoFd, OwnedFd};
 #[cfg(not(any(
     target_os = "android",
     target_os = "emscripten",
@@ -25,7 +26,7 @@ use std::{convert::TryInto, ffi::CStr, io, ptr};
 use std::{ffi::CString, mem::MaybeUninit};
 use unsafe_io::{
     os::posish::{AsRawFd, IntoRawFd, RawFd},
-    IntoUnsafeHandle, OwnsRaw,
+    OwnsRaw,
 };
 
 /// `DIR*`
@@ -35,8 +36,8 @@ pub struct Dir(ptr::NonNull<libc::DIR>);
 impl Dir {
     /// Construct a `Dir`, assuming ownership of the file descriptor.
     #[inline]
-    pub fn from<F: IntoUnsafeHandle>(fd: F) -> io::Result<Self> {
-        let fd = fd.into_unsafe_handle().into_raw_fd();
+    pub fn from<F: IntoFd>(fd: F) -> io::Result<Self> {
+        let fd = fd.into_fd();
         unsafe { Self::_from(fd) }
     }
 
@@ -48,18 +49,19 @@ impl Dir {
     /// itself doesn't guarantee that the handle is valid. Callers must ensure
     /// that the handle is valid.
     #[inline]
-    pub unsafe fn from_into_raw_fd<F: IntoRawFd>(fd: F) -> io::Result<Self> {
-        let fd = fd.into_raw_fd();
+    pub unsafe fn from_into_raw_fd<F: IntoFd>(fd: F) -> io::Result<Self> {
+        let fd = fd.into_fd();
         Self::_from(fd)
     }
 
-    unsafe fn _from(fd: RawFd) -> io::Result<Self> {
-        let d = libc::fdopendir(fd as libc::c_int);
+    unsafe fn _from(fd: OwnedFd) -> io::Result<Self> {
+        let raw = fd.into_raw_fd() as libc::c_int;
+        let d = libc::fdopendir(raw);
         if let Some(d) = ptr::NonNull::new(d) {
             Ok(Self(d))
         } else {
             let e = io::Error::last_os_error();
-            libc::close(fd as libc::c_int);
+            libc::close(raw);
             Err(e)
         }
     }

src/fs/fcopyfile.rs
@@ -1,6 +1,7 @@
 use crate::{fs::CopyfileFlags, negative_err};
+use io_experiment::{AsFd, BorrowedFd};
 use std::{io, mem::MaybeUninit};
-use unsafe_io::{os::posish::AsRawFd, AsUnsafeHandle, UnsafeHandle};
+use unsafe_io::os::posish::AsRawFd;
 
 /// `copyfile_state_t`
 #[allow(non_camel_case_types)]
@@ -12,20 +13,20 @@ type copyfile_flags_t = u32;
 
 /// `fcopyfile(from, to, state, flags)`
 #[inline]
-pub fn fcopyfile<FromFd: AsUnsafeHandle, ToFd: AsUnsafeHandle>(
+pub fn fcopyfile<FromFd: AsFd, ToFd: AsFd>(
     from: &FromFd,
     to: &ToFd,
     state: copyfile_state_t,
     flags: CopyfileFlags,
 ) -> io::Result<()> {
-    let from = from.as_unsafe_handle();
-    let to = to.as_unsafe_handle();
+    let from = from.as_fd();
+    let to = to.as_fd();
     unsafe { _fcopyfile(from, to, state, flags) }
 }
 
 unsafe fn _fcopyfile(
-    from: UnsafeHandle,
-    to: UnsafeHandle,
+    from: BorrowedFd<'_>,
+    to: BorrowedFd<'_>,
     state: copyfile_state_t,
     flags: CopyfileFlags,
 ) -> io::Result<()> {

src/fs/pv.rs
@@ -1,6 +1,7 @@
 //! Positioned *and* vectored I/O: `preadv` and `pwritev`.
 
 use crate::negone_err;
+use io_experiment::{AsFd, BorrowedFd};
 #[cfg(not(any(target_os = "android", target_os = "linux", target_os = "emscripten")))]
 use libc::{preadv as libc_preadv, pwritev as libc_pwritev};
 #[cfg(any(target_os = "android", target_os = "linux", target_os = "emscripten"))]
@@ -12,16 +13,16 @@ use std::{
     convert::TryInto,
     io::{self, IoSlice, IoSliceMut},
 };
-use unsafe_io::{os::posish::AsRawFd, AsUnsafeHandle, UnsafeHandle};
+use unsafe_io::os::posish::AsRawFd;
 
 /// `preadv(fd, bufs.as_ptr(), bufs.len(), offset)`
 #[inline]
-pub fn preadv<Fd: AsUnsafeHandle>(fd: &Fd, bufs: &[IoSliceMut], offset: u64) -> io::Result<usize> {
-    let fd = fd.as_unsafe_handle();
+pub fn preadv<Fd: AsFd>(fd: &Fd, bufs: &[IoSliceMut], offset: u64) -> io::Result<usize> {
+    let fd = fd.as_fd();
     unsafe { _preadv(fd, bufs, offset) }
 }
 
-unsafe fn _preadv(fd: UnsafeHandle, bufs: &[IoSliceMut], offset: u64) -> io::Result<usize> {
+unsafe fn _preadv(fd: BorrowedFd<'_>, bufs: &[IoSliceMut], offset: u64) -> io::Result<usize> {
     let offset = offset
         .try_into()
         .map_err(|_overflow_err| io::Error::from_raw_os_error(libc::EOVERFLOW))?;
@@ -36,12 +37,12 @@ unsafe fn _preadv(fd: UnsafeHandle, bufs: &[IoSliceMut], offset: u64) -> io::Res
 
 /// `pwritev(fd, bufs.as_ptr(), bufs.len(), offset)`
 #[inline]
-pub fn pwritev<Fd: AsUnsafeHandle>(fd: &Fd, bufs: &[IoSlice], offset: u64) -> io::Result<usize> {
-    let fd = fd.as_unsafe_handle();
+pub fn pwritev<Fd: AsFd>(fd: &Fd, bufs: &[IoSlice], offset: u64) -> io::Result<usize> {
+    let fd = fd.as_fd();
     unsafe { _pwritev(fd, bufs, offset) }
 }
 
-unsafe fn _pwritev(fd: UnsafeHandle, bufs: &[IoSlice], offset: u64) -> io::Result<usize> {
+unsafe fn _pwritev(fd: BorrowedFd<'_>, bufs: &[IoSlice], offset: u64) -> io::Result<usize> {
     let offset = offset
         .try_into()
         .map_err(|_overflow_err| io::Error::from_raw_os_error(libc::EOVERFLOW))?;

src/fs/rdadvise.rs
@@ -1,14 +1,15 @@
 use crate::zero_ok;
+use io_experiment::{AsFd, BorrowedFd};
 use std::{convert::TryInto, io};
-use unsafe_io::{os::posish::AsRawFd, AsUnsafeHandle, UnsafeHandle};
+use unsafe_io::os::posish::AsRawFd;
 
 /// `fcntl(fd, F_RDADVISE, radvisory { offset, len })`
-pub fn rdadvise<Fd: AsUnsafeHandle>(fd: &Fd, offset: u64, len: u64) -> io::Result<()> {
-    let fd = fd.as_unsafe_handle();
+pub fn rdadvise<Fd: AsFd>(fd: &Fd, offset: u64, len: u64) -> io::Result<()> {
+    let fd = fd.as_fd();
     unsafe { _rdadvise(fd, offset, len) }
 }
 
-unsafe fn _rdadvise(fd: UnsafeHandle, offset: u64, len: u64) -> io::Result<()> {
+unsafe fn _rdadvise(fd: BorrowedFd<'_>, offset: u64, len: u64) -> io::Result<()> {
     // From the macOS `fcntl` man page:
     // `F_RDADVISE` - Issue an advisory read async with no copy to user.
     //

src/net/socket.rs
@@ -1,9 +1,10 @@
 use crate::zero_ok;
+use io_experiment::{AsFd, BorrowedFd};
 use std::{
     io,
     mem::{size_of, MaybeUninit},
 };
-use unsafe_io::{os::posish::AsRawFd, AsUnsafeHandle, UnsafeHandle};
+use unsafe_io::os::posish::AsRawFd;
 
 /// `SOCK_*` constants.
 #[derive(Debug, Clone, Copy)]
@@ -27,12 +28,12 @@ pub enum SocketType {
 
 /// `getsockopt(fd, SOL_SOCKET, SO_TYPE)`
 #[inline]
-pub fn socket_type<Fd: AsUnsafeHandle>(fd: &Fd) -> io::Result<SocketType> {
-    let fd = fd.as_unsafe_handle();
+pub fn socket_type<Fd: AsFd>(fd: &Fd) -> io::Result<SocketType> {
+    let fd = fd.as_fd();
     unsafe { _socket_type(fd) }
 }
 
-unsafe fn _socket_type(fd: UnsafeHandle) -> io::Result<SocketType> {
+unsafe fn _socket_type(fd: BorrowedFd<'_>) -> io::Result<SocketType> {
     let mut buffer = MaybeUninit::<SocketType>::uninit();
     let mut out_len = size_of::<SocketType>() as libc::socklen_t;
     zero_ok(libc::getsockopt(

bytecodealliance/rustix

src/fs/copy_file_range.rs
@@ -1,25 +1,26 @@
 use crate::negone_err;
+use io_lifetimes::{AsFd, BorrowedFd};
 use std::{convert::TryInto, io, mem, ptr};
-use unsafe_io::{os::posish::AsRawFd, AsUnsafeHandle, UnsafeHandle};
+use unsafe_io::os::posish::AsRawFd;
 
 /// `copy_file_range(fd_in, off_in, fd_out, off_out, len, 0)`
 #[inline]
-pub fn copy_file_range<InUnsafeHandle: AsUnsafeHandle, OutUnsafeHandle: AsUnsafeHandle>(
-    fd_in: &InUnsafeHandle,
+pub fn copy_file_range<InFd: AsFd, OutFd: AsFd>(
+    fd_in: &InFd,
     off_in: Option<&mut u64>,
-    fd_out: &OutUnsafeHandle,
+    fd_out: &OutFd,
     off_out: Option<&mut u64>,
     len: u64,
 ) -> io::Result<u64> {
-    let fd_in = fd_in.as_unsafe_handle();
-    let fd_out = fd_out.as_unsafe_handle();
+    let fd_in = fd_in.as_fd();
+    let fd_out = fd_out.as_fd();
     unsafe { _copy_file_range(fd_in, off_in, fd_out, off_out, len) }
 }
 
 unsafe fn _copy_file_range(
-    fd_in: UnsafeHandle,
+    fd_in: BorrowedFd<'_>,
     off_in: Option<&mut u64>,
-    fd_out: UnsafeHandle,
+    fd_out: BorrowedFd<'_>,
     off_out: Option<&mut u64>,
     len: u64,
 ) -> io::Result<u64> {

src/fs/dir.rs
@@ -4,6 +4,7 @@
 use crate::fs::android::{seekdir as libc_seekdir, telldir as libc_telldir};
 use crate::fs::FileType;
 use errno::{set_errno, Errno};
+use io_lifetimes::{IntoFd, OwnedFd};
 #[cfg(not(any(
     target_os = "android",
     target_os = "emscripten",
@@ -25,7 +26,7 @@ use std::{convert::TryInto, ffi::CStr, io, ptr};
 use std::{ffi::CString, mem::MaybeUninit};
 use unsafe_io::{
     os::posish::{AsRawFd, IntoRawFd, RawFd},
-    IntoUnsafeHandle, OwnsRaw,
+    OwnsRaw,
 };
 
 /// `DIR*`
@@ -35,8 +36,8 @@ pub struct Dir(ptr::NonNull<libc::DIR>);
 impl Dir {
     /// Construct a `Dir`, assuming ownership of the file descriptor.
     #[inline]
-    pub fn from<F: IntoUnsafeHandle>(fd: F) -> io::Result<Self> {
-        let fd = fd.into_unsafe_handle().into_raw_fd();
+    pub fn from<F: IntoFd>(fd: F) -> io::Result<Self> {
+        let fd = fd.into_fd();
         unsafe { Self::_from(fd) }
     }
 
@@ -48,18 +49,19 @@ impl Dir {
     /// itself doesn't guarantee that the handle is valid. Callers must ensure
     /// that the handle is valid.
     #[inline]
-    pub unsafe fn from_into_raw_fd<F: IntoRawFd>(fd: F) -> io::Result<Self> {
-        let fd = fd.into_raw_fd();
+    pub unsafe fn from_into_raw_fd<F: IntoFd>(fd: F) -> io::Result<Self> {
+        let fd = fd.into_fd();
         Self::_from(fd)
     }
 
-    unsafe fn _from(fd: RawFd) -> io::Result<Self> {
-        let d = libc::fdopendir(fd as libc::c_int);
+    unsafe fn _from(fd: OwnedFd) -> io::Result<Self> {
+        let raw = fd.into_raw_fd() as libc::c_int;
+        let d = libc::fdopendir(raw);
         if let Some(d) = ptr::NonNull::new(d) {
             Ok(Self(d))
         } else {
             let e = io::Error::last_os_error();
-            libc::close(fd as libc::c_int);
+            libc::close(raw);
             Err(e)
         }
     }

src/fs/fcopyfile.rs
@@ -1,6 +1,7 @@
 use crate::{fs::CopyfileFlags, negative_err};
+use io_lifetimes::{AsFd, BorrowedFd};
 use std::{io, mem::MaybeUninit};
-use unsafe_io::{os::posish::AsRawFd, AsUnsafeHandle, UnsafeHandle};
+use unsafe_io::os::posish::AsRawFd;
 
 /// `copyfile_state_t`
 #[allow(non_camel_case_types)]
@@ -12,20 +13,20 @@ type copyfile_flags_t = u32;
 
 /// `fcopyfile(from, to, state, flags)`
 #[inline]
-pub fn fcopyfile<FromFd: AsUnsafeHandle, ToFd: AsUnsafeHandle>(
+pub fn fcopyfile<FromFd: AsFd, ToFd: AsFd>(
     from: &FromFd,
     to: &ToFd,
     state: copyfile_state_t,
     flags: CopyfileFlags,
 ) -> io::Result<()> {
-    let from = from.as_unsafe_handle();
-    let to = to.as_unsafe_handle();
+    let from = from.as_fd();
+    let to = to.as_fd();
     unsafe { _fcopyfile(from, to, state, flags) }
 }
 
 unsafe fn _fcopyfile(
-    from: UnsafeHandle,
-    to: UnsafeHandle,
+    from: BorrowedFd<'_>,
+    to: BorrowedFd<'_>,
     state: copyfile_state_t,
     flags: CopyfileFlags,
 ) -> io::Result<()> {

src/fs/pv.rs
@@ -1,6 +1,7 @@
 //! Positioned *and* vectored I/O: `preadv` and `pwritev`.
 
 use crate::negone_err;
+use io_lifetimes::{AsFd, BorrowedFd};
 #[cfg(not(any(target_os = "android", target_os = "linux", target_os = "emscripten")))]
 use libc::{preadv as libc_preadv, pwritev as libc_pwritev};
 #[cfg(any(target_os = "android", target_os = "linux", target_os = "emscripten"))]
@@ -12,16 +13,16 @@ use std::{
     convert::TryInto,
     io::{self, IoSlice, IoSliceMut},
 };
-use unsafe_io::{os::posish::AsRawFd, AsUnsafeHandle, UnsafeHandle};
+use unsafe_io::os::posish::AsRawFd;
 
 /// `preadv(fd, bufs.as_ptr(), bufs.len(), offset)`
 #[inline]
-pub fn preadv<Fd: AsUnsafeHandle>(fd: &Fd, bufs: &[IoSliceMut], offset: u64) -> io::Result<usize> {
-    let fd = fd.as_unsafe_handle();
+pub fn preadv<Fd: AsFd>(fd: &Fd, bufs: &[IoSliceMut], offset: u64) -> io::Result<usize> {
+    let fd = fd.as_fd();
     unsafe { _preadv(fd, bufs, offset) }
 }
 
-unsafe fn _preadv(fd: UnsafeHandle, bufs: &[IoSliceMut], offset: u64) -> io::Result<usize> {
+unsafe fn _preadv(fd: BorrowedFd<'_>, bufs: &[IoSliceMut], offset: u64) -> io::Result<usize> {
     let offset = offset
         .try_into()
         .map_err(|_overflow_err| io::Error::from_raw_os_error(libc::EOVERFLOW))?;
@@ -36,12 +37,12 @@ unsafe fn _preadv(fd: UnsafeHandle, bufs: &[IoSliceMut], offset: u64) -> io::Res
 
 /// `pwritev(fd, bufs.as_ptr(), bufs.len(), offset)`
 #[inline]
-pub fn pwritev<Fd: AsUnsafeHandle>(fd: &Fd, bufs: &[IoSlice], offset: u64) -> io::Result<usize> {
-    let fd = fd.as_unsafe_handle();
+pub fn pwritev<Fd: AsFd>(fd: &Fd, bufs: &[IoSlice], offset: u64) -> io::Result<usize> {
+    let fd = fd.as_fd();
     unsafe { _pwritev(fd, bufs, offset) }
 }
 
-unsafe fn _pwritev(fd: UnsafeHandle, bufs: &[IoSlice], offset: u64) -> io::Result<usize> {
+unsafe fn _pwritev(fd: BorrowedFd<'_>, bufs: &[IoSlice], offset: u64) -> io::Result<usize> {
     let offset = offset
         .try_into()
         .map_err(|_overflow_err| io::Error::from_raw_os_error(libc::EOVERFLOW))?;

src/fs/rdadvise.rs
@@ -1,14 +1,15 @@
 use crate::zero_ok;
+use io_lifetimes::{AsFd, BorrowedFd};
 use std::{convert::TryInto, io};
-use unsafe_io::{os::posish::AsRawFd, AsUnsafeHandle, UnsafeHandle};
+use unsafe_io::os::posish::AsRawFd;
 
 /// `fcntl(fd, F_RDADVISE, radvisory { offset, len })`
-pub fn rdadvise<Fd: AsUnsafeHandle>(fd: &Fd, offset: u64, len: u64) -> io::Result<()> {
-    let fd = fd.as_unsafe_handle();
+pub fn rdadvise<Fd: AsFd>(fd: &Fd, offset: u64, len: u64) -> io::Result<()> {
+    let fd = fd.as_fd();
     unsafe { _rdadvise(fd, offset, len) }
 }
 
-unsafe fn _rdadvise(fd: UnsafeHandle, offset: u64, len: u64) -> io::Result<()> {
+unsafe fn _rdadvise(fd: BorrowedFd<'_>, offset: u64, len: u64) -> io::Result<()> {
     // From the macOS `fcntl` man page:
     // `F_RDADVISE` - Issue an advisory read async with no copy to user.
     //

src/net/socket.rs
@@ -1,9 +1,10 @@
 use crate::zero_ok;
+use io_lifetimes::{AsFd, BorrowedFd};
 use std::{
     io,
     mem::{size_of, MaybeUninit},
 };
-use unsafe_io::{os::posish::AsRawFd, AsUnsafeHandle, UnsafeHandle};
+use unsafe_io::os::posish::AsRawFd;
 
 /// `SOCK_*` constants.
 #[derive(Debug, Clone, Copy)]
@@ -27,12 +28,12 @@ pub enum SocketType {
 
 /// `getsockopt(fd, SOL_SOCKET, SO_TYPE)`
 #[inline]
-pub fn socket_type<Fd: AsUnsafeHandle>(fd: &Fd) -> io::Result<SocketType> {
-    let fd = fd.as_unsafe_handle();
+pub fn socket_type<Fd: AsFd>(fd: &Fd) -> io::Result<SocketType> {
+    let fd = fd.as_fd();
     unsafe { _socket_type(fd) }
 }
 
-unsafe fn _socket_type(fd: UnsafeHandle) -> io::Result<SocketType> {
+unsafe fn _socket_type(fd: BorrowedFd<'_>) -> io::Result<SocketType> {
     let mut buffer = MaybeUninit::<SocketType>::uninit();
     let mut out_len = size_of::<SocketType>() as libc::socklen_t;
     zero_ok(libc::getsockopt(