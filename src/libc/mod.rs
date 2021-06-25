18bde286d73086978cfe849f1ac7014568c7912b
Dan Gohman <dev@sunfishcode.online>
2021-06-24 17:59:37 -0700

    Fix compilation on wasm32-wasi.

--- src/libc/mod.rs
+++ src/libc/mod.rs
@@ -1,5 +1,7 @@
 pub(crate) mod conv;
+#[cfg(not(any(target_os = "wasi", target_os = "redox")))] // WASI doesn't support `net` yet.
 pub(crate) mod sockaddr;
+#[cfg(not(any(target_os = "wasi", target_os = "redox")))] // WASI doesn't support `net` yet.
 pub(crate) mod sockaddr_header;
 
 #[cfg(all(
@@ -23,4 +25,5 @@ pub(crate) use libc::off64_t as libc_off_t;
 ))]
 pub(crate) use libc::off_t as libc_off_t;
 
+#[cfg(not(any(target_os = "redox", target_os = "wasi")))]
 pub use sockaddr::{Ipv4Addr, Ipv6Addr, SocketAddr, SocketAddrUnix, SocketAddrV4, SocketAddrV6};
