9a2988115eeef12a2067cec9f6d3f3e73f4dabf3
Dan Gohman <dev@sunfishcode.online>
2021-06-28 16:17:16 -0700

    Suppress clippy warning about IPv6 constructors taking lots of parameters.

--- src/imp/libc/net/addr.rs
+++ src/imp/libc/net/addr.rs
@@ -33,6 +33,7 @@ pub struct Ipv6Addr(pub(crate) libc::in6_addr);
 
 impl Ipv6Addr {
     /// Construct a new IPv6 address from eight 16-bit segments.
+    #[allow(clippy::many_single_char_names, clippy::too_many_arguments)]
     pub const fn new(a: u16, b: u16, c: u16, d: u16, e: u16, f: u16, g: u16, h: u16) -> Self {
         Self(libc::in6_addr {
             s6_addr: [
