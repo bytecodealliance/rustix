ba3bb3f039be47e248fd9abf57bc666fc90e1dc3
Dan Gohman <dev@sunfishcode.online>
2021-06-29 08:30:11 -0700

    Add a cargo:cargo:rerun-if-env-changed to build.rs to avoid spurious rebuilds.

--- build.rs
+++ build.rs
@@ -2,4 +2,5 @@ fn main() {
     if std::env::var("CARGO_CFG_LINUX_RAW").is_err() {
         println!("cargo:rustc-cfg=libc");
     }
+    println!("cargo:cargo:rerun-if-env-changed=CARGO_CFG_LINUX_RAW");
 }
