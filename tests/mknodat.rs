d673d3619275449e7ab97857c9620b9b2255f6bb
Dan Gohman <dev@sunfishcode.online>
2021-06-29 20:46:40 -0700

    Disable an unsupported mknodat test on FreeBSD.

--- tests/mknodat.rs
+++ tests/mknodat.rs
@@ -11,10 +11,14 @@ fn test_mknodat() {
     let tmp = tempfile::tempdir().unwrap();
     let dir = openat(&cwd(), tmp.path(), OFlags::RDONLY, Mode::empty()).unwrap();
 
-    mknodat(&dir, "foo", Mode::IFREG, 0).unwrap();
-    let stat = statat(&dir, "foo", AtFlags::empty()).unwrap();
-    assert_eq!(FileType::from_raw_mode(stat.st_mode), FileType::RegularFile);
-    unlinkat(&dir, "foo", AtFlags::empty()).unwrap();
+    // Create a regular file. Not supported on FreeBSD.
+    #[cfg(not(target_os = "freebsd"))]
+    {
+        mknodat(&dir, "foo", Mode::IFREG, 0).unwrap();
+        let stat = statat(&dir, "foo", AtFlags::empty()).unwrap();
+        assert_eq!(FileType::from_raw_mode(stat.st_mode), FileType::RegularFile);
+        unlinkat(&dir, "foo", AtFlags::empty()).unwrap();
+    }
 
     mknodat(&dir, "foo", Mode::IFIFO, 0).unwrap();
     let stat = statat(&dir, "foo", AtFlags::empty()).unwrap();
