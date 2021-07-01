22b677c30d0a91bf95cadf607a6c64446cc8b7aa
Dan Gohman <dev@sunfishcode.online>
2021-07-01 12:03:42 -0700

    Work around WASI not having S_IRUSR etc. flags

--- tests/io/mod.rs
+++ tests/io/mod.rs
@@ -1,4 +1,5 @@
 mod isatty;
 mod mmap;
 #[cfg(not(target_os = "redox"))] // redox doesn't have cwd/openat
+#[cfg(not(target_os = "wasi"))] // wasi support for S_IRUSR etc. submitted to libc in #2264
 mod readwrite;
