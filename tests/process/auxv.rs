b0a7fbea0fb3b023b97539539804f95473b0b3fe
Dan Gohman <dev@sunfishcode.online>
2021-09-02 16:22:02 -0700

    Test that rsix's page size matches libc's page size.

--- tests/process/auxv.rs
+++ tests/process/auxv.rs
@@ -8,12 +8,17 @@ fn test_page_size() {
     assert_ne!(size, 0);
     assert!(size.is_power_of_two());
     assert_eq!(size, page_size());
+    assert_eq!(size, unsafe { libc::sysconf(libc::_SC_PAGESIZE) as usize });
 }
 
 #[test]
 #[cfg(any(target_os = "android", target_os = "linux"))]
 fn test_linux_hwcap() {
-    let (_hwcap, _hwcap2) = linux_hwcap();
+    let (_hwcap, hwcap2) = linux_hwcap();
 
-    // These values are architecture-defined.
+    // GLIBC seems to return a different value than `LD_SHOW_AUXV=1` reports.
+    #[cfg(not(target_env = "gnu"))]
+    assert_eq!(_hwcap, unsafe { libc::getauxval(libc::AT_HWCAP) } as usize);
+
+    assert_eq!(hwcap2, unsafe { libc::getauxval(libc::AT_HWCAP2) } as usize);
 }
