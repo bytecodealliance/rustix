e2ddf3619fd9a41b5111c9945e0a0426023a4420
Loïc BRANSTETT <loic.branstett@epitech.eu>
2021-09-15 14:56:31 +0200

    Fix undefined behavior in arm and riscv64 outline assembly

--- src/imp/linux_raw/arch/outline/mod.rs
+++ src/imp/linux_raw/arch/outline/mod.rs
@@ -5,21 +5,23 @@
 //! so that they match with the kernel convention as closely as possible,
 //! to minimize the amount of out-of-line code we need.
 
-#[cfg(target_arch = "arm")]
-mod arm;
-#[cfg(target_arch = "riscv64")]
-mod riscv64;
 #[cfg(target_arch = "x86")]
 mod x86;
-// For x86_64 and aarch64, pass the `nr` argument last.
-#[cfg(any(target_arch = "aarch64", target_arch = "x86_64"))]
+// For riscv64, x86_64, arm and aarch64, pass the `nr` argument last.
+#[cfg(any(
+    target_arch = "riscv64",
+    target_arch = "arm",
+    target_arch = "aarch64",
+    target_arch = "x86_64"
+))]
 mod nr_last;
 
-#[cfg(target_arch = "arm")]
-pub(in crate::imp::linux_raw) use arm::*;
-#[cfg(any(target_arch = "aarch64", target_arch = "x86_64"))]
+#[cfg(any(
+    target_arch = "riscv64",
+    target_arch = "arm",
+    target_arch = "aarch64",
+    target_arch = "x86_64"
+))]
 pub(in crate::imp::linux_raw) use nr_last::*;
-#[cfg(target_arch = "riscv64")]
-pub(in crate::imp::linux_raw) use riscv64::*;
 #[cfg(target_arch = "x86")]
 pub(in crate::imp::linux_raw) use x86::*;
