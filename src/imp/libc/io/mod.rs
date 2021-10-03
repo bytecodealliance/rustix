ea40848728d19c089c168647642ceb137ac10c6e
Dan Gohman <dev@sunfishcode.online>
2021-10-03 08:37:18 -0700

    One more place.

--- src/imp/libc/io/mod.rs
+++ src/imp/libc/io/mod.rs
@@ -8,6 +8,8 @@ pub use error::Error;
 pub use poll_fd::{PollFd, PollFlags};
 #[cfg(not(any(target_os = "redox", target_os = "wasi")))]
 pub use types::Advice;
+#[cfg(target_os = "linux")]
+pub use types::MremapFlags;
 #[cfg(all(
     libc,
     not(any(target_os = "ios", target_os = "macos", target_os = "wasi"))
@@ -18,7 +20,7 @@ pub use types::PIPE_BUF;
 #[cfg(not(target_os = "wasi"))]
 pub use types::{DupFlags, MapFlags, MprotectFlags, ProtFlags, Tcflag, Termios, Winsize, ICANON};
 #[cfg(any(target_os = "android", target_os = "linux"))]
-pub use types::{EventfdFlags, MlockFlags, MremapFlags, ReadWriteFlags, UserfaultfdFlags};
+pub use types::{EventfdFlags, MlockFlags, ReadWriteFlags, UserfaultfdFlags};
 
 use libc::c_int;
 
