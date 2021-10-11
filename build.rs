aa18327c37a859cff621abc0067e589eb6c616e5
Dan Gohman <dev@sunfishcode.online>
2021-10-11 06:39:17 -0700

    Check in pre-assembled libraries for the outline asm code.
    Check in pre-assembled libraries for the outline asm code, allowing the
    "cc" dependency to be optional. This allows rsix to be built on hosts
    that don't have a C compiler installed.
    
    `feature = "cc"` enables the use of the `cc` package, and builds the
    libraries from source, including checking that the built versions match
    the checked-in versions, and is enabled in CI.
    
    Fixes #78.

--- build.rs
+++ build.rs
@@ -1,14 +1,15 @@
+#[cfg(feature = "cc")]
 use cc::Build;
 use std::env::var;
 
+const OUTLINE_PATH: &str = "src/imp/linux_raw/arch/outline";
+
 fn main() {
-    let asm_name = format!(
-        "src/imp/linux_raw/arch/outline/{}.S",
-        var("CARGO_CFG_TARGET_ARCH").unwrap()
-    );
+    let arch = var("CARGO_CFG_TARGET_ARCH").unwrap();
+    let asm_name = format!("{}/{}.S", OUTLINE_PATH, arch);
     let os_name = var("CARGO_CFG_TARGET_OS").unwrap();
-    let is_x32 = var("CARGO_CFG_TARGET_ARCH").unwrap() == "x86_64"
-        && var("CARGO_CFG_TARGET_POINTER_WIDTH").unwrap() == "32";
+    let is_x32 = arch == "x86_64" && var("CARGO_CFG_TARGET_POINTER_WIDTH").unwrap() == "32";
+    println!("cargo:rerun-if-env-changed=CARGO_CFG_TARGET_ARCH");
 
     // If rsix_use_libc is set, or if we're on an architecture/OS that doesn't
     // have raw syscall support, use libc.
@@ -28,13 +29,53 @@ fn main() {
             println!("cargo:rustc-cfg=linux_raw_inline_asm");
             println!("cargo:rustc-cfg=rustc_attrs");
         } else {
-            Build::new().file(&asm_name).compile("asm");
-            println!("cargo:rerun-if-changed={}", asm_name);
-            println!("cargo:rerun-if-env-changed=CARGO_CFG_TARGET_ARCH");
+            link_in_librsix_outline(&arch, &asm_name);
         }
         if rustc_version::version().unwrap() >= rustc_version::Version::parse("1.56.0").unwrap() {
             println!("cargo:rustc-cfg=const_fn_union");
         }
     }
     println!("cargo:rerun-if-env-changed=CARGO_CFG_RSIX_USE_LIBC");
+}
+
+fn link_in_librsix_outline(arch: &str, asm_name: &str) {
+    let name = format!("rsix_outline_{}", arch);
+    let to = format!("{}/lib{}.a", OUTLINE_PATH, name);
+    println!("cargo:rerun-if-changed={}", to);
+
+    // If "cc" is not enabled, use a pre-built library.
+    #[cfg(not(feature = "cc"))]
+    {
+        let _ = asm_name;
+        println!("cargo:rustc-link-search={}", OUTLINE_PATH);
+        println!("cargo:rustc-link-lib=static={}", name);
+    }
+
+    // If "cc" is enabled, build the library from source, update the pre-built
+    // version, and assert that the pre-built version is checked in.
+    #[cfg(feature = "cc")]
+    {
+        let out_dir = var("OUT_DIR").unwrap();
+        Build::new().file(&asm_name).compile(&name);
+        println!("cargo:rerun-if-changed={}", asm_name);
+        let from = format!("{}/lib{}.a", out_dir, name);
+        let prev_metadata = std::fs::metadata(&to);
+        std::fs::copy(&from, &to).unwrap();
+        assert!(
+            prev_metadata.is_ok(),
+            "{} didn't previously exist; please inspect the new file and `git add` it",
+            to
+        );
+        assert!(
+            std::process::Command::new("git")
+                .arg("diff")
+                .arg("--quiet")
+                .arg(&to)
+                .status()
+                .unwrap()
+                .success(),
+            "{} changed; please inspect the change and `git commit` it",
+            to
+        );
+    }
 }
