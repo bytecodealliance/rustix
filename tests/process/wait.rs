4a1bcf255c34c808e8987eeb9066237ca71d00d9
Niv Kaminer <nivkner@zoho.com>
2021-10-23 21:20:31 +0300

    reintroduce test_wait and serialize the tests

--- tests/process/wait.rs
+++ tests/process/wait.rs
@@ -1,9 +1,15 @@
 use libc::{kill, SIGSTOP};
 use rsix::process;
+use serial_test::serial;
 use std::process::Command;
 use std::process::Stdio;
 
+// these tests must execute serially to prevent race condition,
+// where `test_wait` waits for the child process spawned in `test_waitpid`,
+// causing the tests to get stuck.
+
 #[test]
+#[serial]
 fn test_waitpid() {
     let child = Command::new("yes")
         .stdout(Stdio::null())
@@ -17,4 +23,22 @@ fn test_waitpid() {
         .expect("failed to wait")
         .unwrap();
     assert!(status.stopped());
+}
+
+#[test]
+#[serial]
+fn test_wait() {
+    let child = Command::new("yes")
+        .stdout(Stdio::null())
+        .stderr(Stdio::null())
+        .spawn()
+        .expect("failed to execute child");
+    unsafe { kill(child.id() as _, SIGSTOP) };
+
+    let pid = unsafe { process::Pid::from_raw(child.id() as _) };
+    let (child_pid, status) = process::wait(process::WaitOptions::UNTRACED)
+        .expect("failed to wait")
+        .unwrap();
+    assert!(status.stopped());
+    assert_eq!(child_pid, pid);
 }
