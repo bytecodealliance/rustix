d4611fb23e4b015605ee747ac00ee28800ae4e8b
Dan Gohman <dev@sunfishcode.online>
2021-11-18 10:18:02 -0800

    Define flags for `AT_STATX_SYNC_AS_STAT` etc.
    `statx` uses several dedicated `AtFlags` values: `AT_STATX_SYNC_AS_STAT`,
    `AT_STATX_FORCE_SYNC`, and `AT_STATX_DONT_SYNC`. Define `AtFlags`
    bindings for them.

--- src/imp/libc/fs/types.rs
+++ src/imp/libc/fs/types.rs
@@ -57,6 +57,18 @@ bitflags! {
         /// `AT_EACCESS`
         #[cfg(not(any(target_os = "emscripten", target_os = "android")))]
         const EACCESS = c::AT_EACCESS;
+
+        /// `AT_STATX_SYNC_AS_STAT`
+        #[cfg(all(target_os = "linux", target_env = "gnu"))]
+        const STATX_SYNC_AS_STAT = linux_raw_sys::general::AT_STATX_SYNC_AS_STAT;
+
+        /// `AT_STATX_FORCE_SYNC`
+        #[cfg(all(target_os = "linux", target_env = "gnu"))]
+        const STATX_FORCE_SYNC = linux_raw_sys::general::AT_STATX_FORCE_SYNC;
+
+        /// `AT_STATX_DONT_SYNC`
+        #[cfg(all(target_os = "linux", target_env = "gnu"))]
+        const STATX_DONT_SYNC = linux_raw_sys::general::AT_STATX_DONT_SYNC;
     }
 }
 
