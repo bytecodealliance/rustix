8fd25d67fcb4224a202e18105a8983bab1ab7fc7
Dan Gohman <dev@sunfishcode.online>
2021-11-18 10:43:03 -0800

    Fix copy+pasto in libc configurations.

--- src/imp/libc/fs/types.rs
+++ src/imp/libc/fs/types.rs
@@ -60,15 +60,15 @@ bitflags! {
 
         /// `AT_STATX_SYNC_AS_STAT`
         #[cfg(all(target_os = "linux", target_env = "gnu"))]
-        const STATX_SYNC_AS_STAT = linux_raw_sys::general::AT_STATX_SYNC_AS_STAT;
+        const STATX_SYNC_AS_STAT = libc::AT_STATX_SYNC_AS_STAT;
 
         /// `AT_STATX_FORCE_SYNC`
         #[cfg(all(target_os = "linux", target_env = "gnu"))]
-        const STATX_FORCE_SYNC = linux_raw_sys::general::AT_STATX_FORCE_SYNC;
+        const STATX_FORCE_SYNC = libc::AT_STATX_FORCE_SYNC;
 
         /// `AT_STATX_DONT_SYNC`
         #[cfg(all(target_os = "linux", target_env = "gnu"))]
-        const STATX_DONT_SYNC = linux_raw_sys::general::AT_STATX_DONT_SYNC;
+        const STATX_DONT_SYNC = libc::AT_STATX_DONT_SYNC;
     }
 }
 
