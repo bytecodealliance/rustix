1d0827a0b3960cfad6bea6d7a9996c9c3ae2e5f8
Dan Gohman <dev@sunfishcode.online>
2022-01-19 13:44:28 -0800

    Fix `readlinkat` syscalls to not be `readonly`.
    The `readlinkat` syscall writes into the output buffer, so it is not
    `readonly`, so use a non-`readonly` syscall wrapper for it. I believe
    this bug isn't observable outside the crate, due to the restricted
    ways that the actual syscall wrapper is used.

--- src/imp/linux_raw/fs/syscalls.rs
+++ src/imp/linux_raw/fs/syscalls.rs
@@ -617,7 +617,7 @@ pub(crate) fn statfs(filename: &ZStr) -> io::Result<StatFs> {
 pub(crate) fn readlink(path: &ZStr, buf: &mut [u8]) -> io::Result<usize> {
     let (buf_addr_mut, buf_len) = slice_mut(buf);
     unsafe {
-        ret_usize(syscall4_readonly(
+        ret_usize(syscall4(
             nr(__NR_readlinkat),
             c_int(AT_FDCWD),
             c_str(path),
@@ -631,7 +631,7 @@ pub(crate) fn readlink(path: &ZStr, buf: &mut [u8]) -> io::Result<usize> {
 pub(crate) fn readlinkat(dirfd: BorrowedFd<'_>, path: &ZStr, buf: &mut [u8]) -> io::Result<usize> {
     let (buf_addr_mut, buf_len) = slice_mut(buf);
     unsafe {
-        ret_usize(syscall4_readonly(
+        ret_usize(syscall4(
             nr(__NR_readlinkat),
             borrowed_fd(dirfd),
             c_str(path),
