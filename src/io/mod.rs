d1c307863e14d48a3f769dcebdfd5fa980e248b4
Mek101 <mek101-dev.inv@slmail.me>
2022-01-24 16:46:34 +0100

    Add ioctl_blk*szget android support.

--- src/io/mod.rs
+++ src/io/mod.rs
@@ -54,7 +54,7 @@ pub use ioctl::ioctl_fioclex;
 pub use ioctl::ioctl_fionbio;
 #[cfg(not(any(windows, target_os = "redox")))]
 pub use ioctl::ioctl_fionread;
-#[cfg(target_os = "linux")]
+#[cfg(any(target_os = "android", target_os = "linux"))]
 pub use ioctl::{ioctl_blkpbszget, ioctl_blksszget};
 #[cfg(not(any(windows, target_os = "wasi")))]
 pub use ioctl::{ioctl_tcgets, ioctl_tiocgwinsz};
