95b301b211584722851ab9aa33b6cae07827f3e9
Dan Gohman <dev@sunfishcode.online>
2022-02-15 09:39:22 -0800

    Minor code simplification using `cfg_attr`.

--- src/imp/linux_raw/arch/inline/mod.rs
+++ src/imp/linux_raw/arch/inline/mod.rs
@@ -2,36 +2,14 @@
 //! much like regular calls, with custom calling conventions, and calling
 //! conventions are otherwise the compiler's job. But for now, use inline asm.
 
-#[cfg(target_arch = "aarch64")]
-mod aarch64;
-#[cfg(target_arch = "arm")]
-mod arm;
-#[cfg(target_arch = "mips")]
-mod mips;
-#[cfg(target_arch = "mips64")]
-mod mips64;
-#[cfg(target_arch = "powerpc64")]
-mod powerpc64;
-#[cfg(target_arch = "riscv64")]
-mod riscv64;
-#[cfg(target_arch = "x86")]
-mod x86;
-#[cfg(target_arch = "x86_64")]
-mod x86_64;
+#[cfg_attr(target_arch = "aarch64", path = "aarch64.rs")]
+#[cfg_attr(target_arch = "arm", path = "arm.rs")]
+#[cfg_attr(target_arch = "mips", path = "mips.rs")]
+#[cfg_attr(target_arch = "mips64", path = "mips64.rs")]
+#[cfg_attr(target_arch = "powerpc64", path = "powerpc64.rs")]
+#[cfg_attr(target_arch = "riscv64", path = "riscv64.rs")]
+#[cfg_attr(target_arch = "x86", path = "x86.rs")]
+#[cfg_attr(target_arch = "x86_64", path = "x86_64.rs")]
+mod target_arch;
 
-#[cfg(target_arch = "aarch64")]
-pub(in crate::imp) use self::aarch64::*;
-#[cfg(target_arch = "arm")]
-pub(in crate::imp) use self::arm::*;
-#[cfg(target_arch = "mips")]
-pub(in crate::imp) use self::mips::*;
-#[cfg(target_arch = "mips64")]
-pub(in crate::imp) use self::mips64::*;
-#[cfg(target_arch = "powerpc64")]
-pub(in crate::imp) use self::powerpc64::*;
-#[cfg(target_arch = "riscv64")]
-pub(in crate::imp) use self::riscv64::*;
-#[cfg(target_arch = "x86")]
-pub(in crate::imp) use self::x86::*;
-#[cfg(target_arch = "x86_64")]
-pub(in crate::imp) use self::x86_64::*;
+pub(in crate::imp) use self::target_arch::*;
