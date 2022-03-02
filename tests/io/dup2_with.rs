74d99dae2db4b4e32da8cc2136c22f71d5e9b132
Dan Gohman <dev@sunfishcode.online>
2022-03-02 14:48:17 -0800

    Fix `dup2` and `dup2_with` inconsistencies.
    When emulating `dup2` using `dup3`, emulate the POSIX `dup2` behavior of
    doing nothing if the file descriptors are equal.
    
    When emulating `dup2_with` using `dup2`, emulate the Linux `dup3` behavior
    of failing if the file descriptors are equal.
    
    We may eventually want to make broader changes to the API; I've filed #244
    to track this.

---
+++ tests/io/dup2_with.rs
@@ -0,0 +1,21 @@
+use rustix::io::DupFlags;
+
+/// `dup2` uses POSIX `dup2` which silently does nothing if the file
+/// descriptors are equal.
+#[test]
+fn test_dup2() {
+    let (a, b) = rustix::io::pipe().unwrap();
+    rustix::io::dup2(&a, &a).unwrap();
+    rustix::io::dup2(&b, &b).unwrap();
+}
+
+/// `dup2_with` uses Linux `dup3` which fails with `INVAL` if the
+/// file descriptors are equal.
+#[test]
+fn test_dup2_with() {
+    let (a, b) = rustix::io::pipe().unwrap();
+    assert_eq!(rustix::io::dup2_with(&a, &a, DupFlags::empty()), Err(rustix::io::Error::INVAL));
+    assert_eq!(rustix::io::dup2_with(&b, &b, DupFlags::empty()), Err(rustix::io::Error::INVAL));
+    assert_eq!(rustix::io::dup2_with(&a, &a, DupFlags::CLOEXEC), Err(rustix::io::Error::INVAL));
+    assert_eq!(rustix::io::dup2_with(&b, &b, DupFlags::CLOEXEC), Err(rustix::io::Error::INVAL));
+}
