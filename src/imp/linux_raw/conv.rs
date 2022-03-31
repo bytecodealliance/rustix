ad0df1dccd532d7f8c034d13cbde82c7faae778a
Dan Gohman <dev@sunfishcode.online>
2022-03-31 07:08:07 -0700

    Use `pass_usize` and other conversion functions to factor out redundant casts.

--- src/imp/linux_raw/conv.rs
+++ src/imp/linux_raw/conv.rs
@@ -8,7 +8,7 @@
 #![allow(unsafe_code)]
 
 use super::c;
-use super::fd::{AsRawFd, BorrowedFd, FromRawFd};
+use super::fd::{AsRawFd, BorrowedFd, FromRawFd, RawFd};
 use super::fs::{FileType, Mode, OFlags};
 #[cfg(not(debug_assertions))]
 use super::io::error::decode_usize_infallible;
@@ -36,41 +36,41 @@ use linux_raw_sys::general::{__kernel_clockid_t, socklen_t};
 #[cfg(target_arch = "x86")]
 #[inline]
 pub(super) fn x86_sys<'a, Num: ArgNumber>(sys: u32) -> ArgReg<'a, Num> {
-    raw_arg(sys as usize as *mut c::c_void)
+    pass_usize(sys as usize)
 }
 
 #[cfg(all(target_endian = "little", target_pointer_width = "32"))]
 #[inline]
 pub(super) fn lo<'a, Num: ArgNumber>(x: u64) -> ArgReg<'a, Num> {
-    raw_arg((x >> 32) as usize as *mut c::c_void)
+    pass_usize((x >> 32) as usize)
 }
 
 #[cfg(all(target_endian = "little", target_pointer_width = "32"))]
 #[inline]
 pub(super) fn hi<'a, Num: ArgNumber>(x: u64) -> ArgReg<'a, Num> {
-    raw_arg((x & 0xffff_ffff) as usize as *mut c::c_void)
+    pass_usize((x & 0xffff_ffff) as usize)
 }
 
 #[cfg(all(target_endian = "big", target_pointer_width = "32"))]
 #[inline]
 pub(super) fn lo<'a, Num: ArgNumber>(x: u64) -> ArgReg<'a, Num> {
-    raw_arg((x & 0xffff_ffff) as usize as *mut c::c_void)
+    pass_usize((x & 0xffff_ffff) as usize)
 }
 
 #[cfg(all(target_endian = "big", target_pointer_width = "32"))]
 #[inline]
 pub(super) fn hi<'a, Num: ArgNumber>(x: u64) -> ArgReg<'a, Num> {
-    raw_arg((x >> 32) as usize as *mut c::c_void)
+    pass_usize((x >> 32) as usize)
 }
 
 #[inline]
 pub(super) fn zero<'a, Num: ArgNumber>() -> ArgReg<'a, Num> {
-    raw_arg(0_usize as *mut c::c_void)
+    raw_arg(null_mut())
 }
 
 #[inline]
 pub(super) fn size_of<'a, T: Sized, Num: ArgNumber>() -> ArgReg<'a, Num> {
-    raw_arg(core::mem::size_of::<T>() as *mut c::c_void)
+    pass_usize(core::mem::size_of::<T>())
 }
 
 #[inline]
@@ -97,34 +97,34 @@ pub(super) fn c_str<'a, Num: ArgNumber>(c: &'a ZStr) -> ArgReg<'a, Num> {
 
 #[inline]
 pub(super) fn opt_c_str<'a, Num: ArgNumber>(t: Option<&'a ZStr>) -> ArgReg<'a, Num> {
-    raw_arg(
-        (match t {
-            Some(s) => s.as_ptr() as *mut u8,
-            None => null_mut(),
-        })
-        .cast(),
-    )
+    raw_arg(match t {
+        Some(s) => {
+            let mut_ptr = s.as_ptr() as *mut u8;
+            mut_ptr.cast()
+        }
+        None => null_mut(),
+    })
 }
 
 #[inline]
 pub(super) fn borrowed_fd<'a, Num: ArgNumber>(fd: BorrowedFd<'a>) -> ArgReg<'a, Num> {
-    // Linux doesn't look at the high bits beyond the `c_int`, so use
-    // zero-extension rather than sign-extension because it's a smaller
-    // instruction.
-    debug_assert!(fd.as_raw_fd() == crate::fs::cwd().as_raw_fd() || fd.as_raw_fd() >= 0);
-    raw_arg(fd.as_raw_fd() as c::c_uint as usize as *mut c::c_void)
+    raw_fd(fd.as_raw_fd())
 }
 
 #[inline]
-pub(super) fn raw_fd<'a, Num: ArgNumber>(fd: c::c_int) -> ArgReg<'a, Num> {
-    // As above, use zero-extension rather than sign-extension.
+pub(super) fn raw_fd<'a, Num: ArgNumber>(fd: RawFd) -> ArgReg<'a, Num> {
     debug_assert!(fd == crate::fs::cwd().as_raw_fd() || fd >= 0);
-    raw_arg(fd as c::c_uint as usize as *mut c::c_void)
+
+    // Linux doesn't look at the high bits beyond the `c_int`, so use
+    // zero-extension rather than sign-extension because it's a smaller
+    // instruction.
+    let fd: c::c_int = fd;
+    pass_usize(fd as c::c_uint as usize)
 }
 
 #[inline]
 pub(super) fn no_fd<'a, Num: ArgNumber>() -> ArgReg<'a, Num> {
-    raw_arg(-1_isize as usize as *mut c::c_void)
+    pass_usize(-1_isize as usize)
 }
 
 #[inline]
@@ -137,18 +137,14 @@ pub(super) fn slice_just_addr<'a, T: Sized, Num: ArgNumber>(v: &'a [T]) -> ArgRe
 pub(super) fn slice<'a, T: Sized, Num0: ArgNumber, Num1: ArgNumber>(
     v: &'a [T],
 ) -> (ArgReg<'a, Num0>, ArgReg<'a, Num1>) {
-    let mut_ptr = v.as_ptr() as *mut T;
-    (raw_arg(mut_ptr.cast()), raw_arg(v.len() as *mut c::c_void))
+    (slice_just_addr(v), pass_usize(v.len()))
 }
 
 #[inline]
 pub(super) fn slice_mut<'a, T: Sized, Num0: ArgNumber, Num1: ArgNumber>(
     v: &mut [T],
 ) -> (ArgReg<'a, Num0>, ArgReg<'a, Num1>) {
-    (
-        raw_arg(v.as_mut_ptr().cast()),
-        raw_arg(v.len() as *mut c::c_void),
-    )
+    (raw_arg(v.as_mut_ptr().cast()), pass_usize(v.len()))
 }
 
 #[inline]
@@ -193,18 +189,18 @@ pub(super) unsafe fn opt_ref<'a, T: Sized, Num: ArgNumber>(t: Option<&'a T>) -> 
 
 #[inline]
 pub(super) fn c_int<'a, Num: ArgNumber>(i: c::c_int) -> ArgReg<'a, Num> {
-    raw_arg(i as usize as *mut c::c_void)
+    pass_usize(i as usize)
 }
 
 #[inline]
 pub(super) fn c_uint<'a, Num: ArgNumber>(i: c::c_uint) -> ArgReg<'a, Num> {
-    raw_arg(i as usize as *mut c::c_void)
+    pass_usize(i as usize)
 }
 
 #[cfg(target_pointer_width = "64")]
 #[inline]
 pub(super) fn loff_t<'a, Num: ArgNumber>(i: __kernel_loff_t) -> ArgReg<'a, Num> {
-    raw_arg(i as usize as *mut c::c_void)
+    pass_usize(i as usize)
 }
 
 #[cfg(target_pointer_width = "64")]
@@ -212,22 +208,22 @@ pub(super) fn loff_t<'a, Num: ArgNumber>(i: __kernel_loff_t) -> ArgReg<'a, Num> 
 pub(super) fn loff_t_from_u64<'a, Num: ArgNumber>(i: u64) -> ArgReg<'a, Num> {
     // `loff_t` is signed, but syscalls which expect `loff_t` return `EINVAL`
     // if it's outside the signed `i64` range, so we can silently cast.
-    raw_arg(i as usize as *mut c::c_void)
+    pass_usize(i as usize)
 }
 
 #[inline]
 pub(super) fn clockid_t<'a, Num: ArgNumber>(i: ClockId) -> ArgReg<'a, Num> {
-    raw_arg(i as __kernel_clockid_t as usize as *mut c::c_void)
+    pass_usize(i as __kernel_clockid_t as usize)
 }
 
 #[inline]
 pub(super) fn socklen_t<'a, Num: ArgNumber>(i: socklen_t) -> ArgReg<'a, Num> {
-    raw_arg(i as usize as *mut c::c_void)
+    pass_usize(i as usize)
 }
 
 #[inline]
 pub(super) fn mode_as<'a, Num: ArgNumber>(mode: Mode) -> ArgReg<'a, Num> {
-    raw_arg(mode.bits() as usize as *mut c::c_void)
+    pass_usize(mode.bits() as usize)
 }
 
 #[inline]
@@ -235,22 +231,20 @@ pub(super) fn mode_and_type_as<'a, Num: ArgNumber>(
     mode: Mode,
     file_type: FileType,
 ) -> ArgReg<'a, Num> {
-    raw_arg((mode.as_raw_mode() as usize | file_type.as_raw_mode() as usize) as *mut c::c_void)
+    pass_usize(mode.as_raw_mode() as usize | file_type.as_raw_mode() as usize)
 }
 
 #[cfg(target_pointer_width = "64")]
 #[inline]
 pub(super) fn dev_t<'a, Num: ArgNumber>(dev: u64) -> ArgReg<'a, Num> {
-    raw_arg(dev as usize as *mut c::c_void)
+    pass_usize(dev as usize)
 }
 
 #[cfg(target_pointer_width = "32")]
 #[inline]
 pub(super) fn dev_t<'a, Num: ArgNumber>(dev: u64) -> io::Result<ArgReg<'a, Num>> {
     use core::convert::TryInto;
-    dev.try_into()
-        .map(|dev: usize| raw_arg(dev as *mut c::c_void))
-        .map_err(|_err| io::Error::INVAL)
+    pass_usize(dev.try_into().map_err(|_err| io::Error::INVAL)?)
 }
 
 #[cfg(target_pointer_width = "32")]
@@ -273,7 +267,7 @@ const fn oflags_bits(oflags: OFlags) -> c::c_uint {
 
 #[inline]
 pub(super) fn oflags<'a, Num: ArgNumber>(oflags: OFlags) -> ArgReg<'a, Num> {
-    raw_arg(oflags_bits(oflags) as usize as *mut c::c_void)
+    pass_usize(oflags_bits(oflags) as usize)
 }
 
 #[inline]
@@ -289,17 +283,17 @@ pub(super) fn resource<'a, Num: ArgNumber>(resource: Resource) -> ArgReg<'a, Num
 
 #[inline]
 pub(super) fn regular_pid<'a, Num: ArgNumber>(pid: Pid) -> ArgReg<'a, Num> {
-    raw_arg(pid.as_raw_nonzero().get() as usize as *mut c::c_void)
+    pass_usize(pid.as_raw_nonzero().get() as usize)
 }
 
 #[inline]
 pub(super) fn negative_pid<'a, Num: ArgNumber>(pid: Pid) -> ArgReg<'a, Num> {
-    raw_arg(pid.as_raw_nonzero().get().wrapping_neg() as usize as *mut c::c_void)
+    pass_usize(pid.as_raw_nonzero().get().wrapping_neg() as usize)
 }
 
 #[inline]
 pub(super) fn signal<'a, Num: ArgNumber>(sig: Signal) -> ArgReg<'a, Num> {
-    raw_arg(sig as usize as *mut c::c_void)
+    pass_usize(sig as usize)
 }
 
 #[inline]
