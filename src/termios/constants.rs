2b490d7c751ed5700572b0356a9bc34e9f7fc2e9
Dan Gohman <dev@sunfishcode.online>
2022-05-02 12:34:06 -0700

    Add a function for converting from a `Speed` to a speed value.

--- src/termios/constants.rs
+++ src/termios/constants.rs
@@ -590,3 +590,130 @@ pub use imp::termios::types::{
     B50, B57600, B600, B75, B9600, ICANON, VDISCARD, VEOF, VEOL, VEOL2, VERASE, VINTR, VKILL,
     VLNEXT, VMIN, VQUIT, VREPRINT, VSTART, VSTOP, VSUSP, VTIME, VWERASE,
 };
+
+/// Translate from a `Speed` code to a speed value `u32`.
+///
+/// ```rust
+/// let speed = speed_value(rustix::termios::B57600);
+/// assert_eq!(speed, 57600);
+/// ```
+pub fn speed_value(speed: imp::termios::types::Speed) -> Option<u32> {
+    match speed {
+        imp::termios::types::B0 => Some(0),
+        imp::termios::types::B50 => Some(50),
+        imp::termios::types::B75 => Some(75),
+        imp::termios::types::B110 => Some(110),
+        imp::termios::types::B134 => Some(134),
+        imp::termios::types::B150 => Some(150),
+        imp::termios::types::B200 => Some(200),
+        imp::termios::types::B300 => Some(300),
+        imp::termios::types::B600 => Some(600),
+        imp::termios::types::B1200 => Some(1200),
+        imp::termios::types::B1800 => Some(1800),
+        imp::termios::types::B2400 => Some(2400),
+        imp::termios::types::B4800 => Some(4800),
+        imp::termios::types::B9600 => Some(9600),
+        imp::termios::types::B19200 => Some(19200),
+        imp::termios::types::B38400 => Some(38400),
+        imp::termios::types::B57600 => Some(57600),
+        imp::termios::types::B115200 => Some(115200),
+        imp::termios::types::B230400 => Some(230400),
+        #[cfg(not(any(target_os = "ios", target_os = "macos", target_os = "openbsd")))]
+        imp::termios::types::B460800 => Some(460800),
+        #[cfg(not(any(
+            target_os = "dragonfly",
+            target_os = "freebsd",
+            target_os = "illumos",
+            target_os = "ios",
+            target_os = "macos",
+            target_os = "netbsd",
+            target_os = "openbsd",
+        )))]
+        imp::termios::types::B500000 => Some(500000),
+        #[cfg(not(any(
+            target_os = "dragonfly",
+            target_os = "freebsd",
+            target_os = "illumos",
+            target_os = "ios",
+            target_os = "macos",
+            target_os = "netbsd",
+            target_os = "openbsd",
+        )))]
+        imp::termios::types::B576000 => Some(576000),
+        #[cfg(not(any(target_os = "ios", target_os = "macos", target_os = "openbsd")))]
+        imp::termios::types::B921600 => Some(921600),
+        #[cfg(not(any(
+            target_os = "dragonfly",
+            target_os = "freebsd",
+            target_os = "ios",
+            target_os = "macos",
+            target_os = "netbsd",
+            target_os = "openbsd",
+        )))]
+        imp::termios::types::B1000000 => Some(1000000),
+        #[cfg(not(any(
+            target_os = "dragonfly",
+            target_os = "freebsd",
+            target_os = "ios",
+            target_os = "macos",
+            target_os = "netbsd",
+            target_os = "openbsd",
+        )))]
+        imp::termios::types::B1152000 => Some(1152000),
+        #[cfg(not(any(
+            target_os = "dragonfly",
+            target_os = "freebsd",
+            target_os = "ios",
+            target_os = "macos",
+            target_os = "netbsd",
+            target_os = "openbsd",
+        )))]
+        imp::termios::types::B1500000 => Some(1500000),
+        #[cfg(not(any(
+            target_os = "dragonfly",
+            target_os = "freebsd",
+            target_os = "ios",
+            target_os = "macos",
+            target_os = "netbsd",
+            target_os = "openbsd",
+        )))]
+        imp::termios::types::B2000000 => Some(2000000),
+        #[cfg(not(any(
+            target_os = "dragonfly",
+            target_os = "freebsd",
+            target_os = "ios",
+            target_os = "macos",
+            target_os = "netbsd",
+            target_os = "openbsd",
+        )))]
+        imp::termios::types::B2500000 => Some(2500000),
+        #[cfg(not(any(
+            target_os = "dragonfly",
+            target_os = "freebsd",
+            target_os = "ios",
+            target_os = "macos",
+            target_os = "netbsd",
+            target_os = "openbsd",
+        )))]
+        imp::termios::types::B3000000 => Some(3000000),
+        #[cfg(not(any(
+            target_os = "dragonfly",
+            target_os = "freebsd",
+            target_os = "ios",
+            target_os = "macos",
+            target_os = "netbsd",
+            target_os = "openbsd",
+        )))]
+        imp::termios::types::B3500000 => Some(3500000),
+        #[cfg(not(any(
+            target_os = "dragonfly",
+            target_os = "freebsd",
+            target_os = "ios",
+            target_os = "macos",
+            target_os = "netbsd",
+            target_os = "openbsd",
+        )))]
+        imp::termios::types::B4000000 => Some(4000000),
+        _ => None,
+    }
+}
