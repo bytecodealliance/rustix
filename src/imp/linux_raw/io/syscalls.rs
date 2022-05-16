916408f0901e6ae33a48f0d0c6e893d4711793a6
Asaf Fisher <asaffisher.dev@gmail.com>
2022-05-16 18:47:32 +0300

    Fix #323: Remove `std` usage in `linux_raw::io::syscalls`

--- src/imp/linux_raw/io/syscalls.rs
+++ src/imp/linux_raw/io/syscalls.rs
@@ -399,7 +399,7 @@ pub(crate) fn dup2(fd: BorrowedFd<'_>, new: &OwnedFd) -> io::Result<()> {
     {
         // `dup3` fails if the old and new file descriptors have the same
         // value, so emulate the `dup2` behavior.
-        use std::os::unix::io::AsRawFd;
+        use crate::fd::AsRawFd;
         if fd.as_raw_fd() == new.as_raw_fd() {
             return Ok(());
         }
