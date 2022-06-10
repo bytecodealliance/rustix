ea738f70164d70d787cb0e7625172ffdebf7425e
Dan Gohman <dev@sunfishcode.online>
2022-06-10 02:22:50 -0700

    Fix types for non-x86 32-bit Android.

--- src/imp/libc/fs/makedev.rs
+++ src/imp/libc/fs/makedev.rs
@@ -1,4 +1,4 @@
-#[cfg(not(all(target_os = "android", target_arch = "x86")))]
+#[cfg(not(all(target_os = "android", target_pointer_width = "32")))]
 use super::super::c;
 use crate::fs::Dev;
 
@@ -8,17 +8,17 @@ pub(crate) fn makedev(maj: u32, min: u32) -> Dev {
     unsafe { c::makedev(maj, min) }
 }
 
-#[cfg(all(target_os = "android", not(target_arch = "x86")))]
+#[cfg(all(target_os = "android", not(target_pointer_width = "32")))]
 #[inline]
 pub(crate) fn makedev(maj: u32, min: u32) -> Dev {
     // Android's `makedev` oddly has signed argument types.
     unsafe { c::makedev(maj as i32, min as i32) }
 }
 
-#[cfg(all(target_os = "android", target_arch = "x86"))]
+#[cfg(all(target_os = "android", target_pointer_width = "32"))]
 #[inline]
 pub(crate) fn makedev(maj: u32, min: u32) -> Dev {
-    // 32-bit x86 Android's `dev_t` is 32-bit, but its `st_dev` is 64-bit,
+    // 32-bit Android's `dev_t` is 32-bit, but its `st_dev` is 64-bit,
     // so we do it ourselves.
     ((u64::from(maj) & 0xffff_f000_u64) << 32)
         | ((u64::from(maj) & 0x0000_0fff_u64) << 8)
@@ -39,17 +39,17 @@ pub(crate) fn major(dev: Dev) -> u32 {
     unsafe { c::major(dev) }
 }
 
-#[cfg(all(target_os = "android", not(target_arch = "x86")))]
+#[cfg(all(target_os = "android", not(target_pointer_width = "32")))]
 #[inline]
 pub(crate) fn major(dev: Dev) -> u32 {
     // Android's `major` oddly has signed return types.
     (unsafe { c::major(dev) }) as u32
 }
 
-#[cfg(all(target_os = "android", target_arch = "x86"))]
+#[cfg(all(target_os = "android", target_pointer_width = "32"))]
 #[inline]
 pub(crate) fn major(dev: Dev) -> u32 {
-    // 32-bit x86 Android's `dev_t` is 32-bit, but its `st_dev` is 64-bit,
+    // 32-bit Android's `dev_t` is 32-bit, but its `st_dev` is 64-bit,
     // so we do it ourselves.
     (((dev >> 31 >> 1) & 0xffff_f000) | ((dev >> 8) & 0x0000_0fff)) as u32
 }
@@ -67,17 +67,17 @@ pub(crate) fn minor(dev: Dev) -> u32 {
     unsafe { c::minor(dev) }
 }
 
-#[cfg(all(target_os = "android", not(target_arch = "x86")))]
+#[cfg(all(target_os = "android", not(target_pointer_width = "32")))]
 #[inline]
 pub(crate) fn minor(dev: Dev) -> u32 {
     // Android's `minor` oddly has signed return types.
     (unsafe { c::minor(dev) }) as u32
 }
 
-#[cfg(all(target_os = "android", target_arch = "x86"))]
+#[cfg(all(target_os = "android", target_pointer_width = "32"))]
 #[inline]
 pub(crate) fn minor(dev: Dev) -> u32 {
-    // 32-bit x86 Android's `dev_t` is 32-bit, but its `st_dev` is 64-bit,
+    // 32-bit Android's `dev_t` is 32-bit, but its `st_dev` is 64-bit,
     // so we do it ourselves.
     (((dev >> 12) & 0xffff_ff00) | (dev & 0x0000_00ff)) as u32
 }
