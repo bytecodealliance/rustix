dd4c9fadf145243433978d5894f3f0b19af65ff9
Dan Gohman <dev@sunfishcode.online>
2022-06-10 17:52:55 -0700

    Add `c_char` to `rustix::ffi`.

--- src/ffi/mod.rs
+++ src/ffi/mod.rs
@@ -3,7 +3,9 @@
 #[cfg(not(feature = "std"))]
 pub use alloc::ffi::{CString, NulError};
 #[cfg(not(feature = "std"))]
-pub use core::ffi::{CStr, FromBytesWithNulError};
+pub use core::ffi::{c_char, CStr, FromBytesWithNulError};
 
 #[cfg(feature = "std")]
 pub use std::ffi::{CStr, CString, FromBytesWithNulError, NulError};
+#[cfg(feature = "std")]
+pub use std::os::raw::c_char;
