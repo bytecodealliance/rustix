4db9ba98297427b0b928e73e2607919b39db65b1
Dan Gohman <dev@sunfishcode.online>
2022-06-13 15:52:07 -0700

    Reorganize some code and add comments.

--- src/ffi/mod.rs
+++ src/ffi/mod.rs
@@ -1,11 +1,16 @@
 //! Utilities related to FFI bindings.
 
-#[cfg(not(feature = "std"))]
-pub use alloc::ffi::{CString, NulError};
-#[cfg(not(feature = "std"))]
-pub use core::ffi::{c_char, CStr, FromBytesWithNulError};
+// If we have std, use it.
+#[cfg(feature = "std")]
+pub use {
+    std::ffi::{CStr, CString, FromBytesWithNulError, NulError},
+    std::os::raw::c_char,
+};
 
-#[cfg(feature = "std")]
-pub use std::ffi::{CStr, CString, FromBytesWithNulError, NulError};
-#[cfg(feature = "std")]
-pub use std::os::raw::c_char;
+// If we don't have std, we can depend on core and alloc having these features
+// in new versions of Rust.
+#[cfg(not(feature = "std"))]
+pub use {
+    alloc::ffi::{CString, NulError},
+    core::ffi::{c_char, CStr, FromBytesWithNulError},
+};
