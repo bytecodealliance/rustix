d6fb13656df191ae8a03ce15a817cc1d276fdc53
Niv Kaminer <nivkner@zoho.com>
2022-09-23 00:10:03 +0300

    add splice function

--- src/io/pipe.rs
+++ src/io/pipe.rs
@@ -1,9 +1,13 @@
 use crate::backend;
 use crate::fd::OwnedFd;
 use crate::io;
+use backend::fd::AsFd;
 
 #[cfg(not(any(target_os = "ios", target_os = "macos")))]
 pub use backend::io::types::PipeFlags;
+
+#[cfg(any(target_os = "android", target_os = "linux"))]
+pub use backend::io::types::SpliceFlags;
 
 /// `PIPE_BUF`—The maximum length at which writes to a pipe are atomic.
 ///
@@ -52,4 +56,18 @@ pub fn pipe() -> io::Result<(OwnedFd, OwnedFd)> {
 #[doc(alias = "pipe2")]
 pub fn pipe_with(flags: PipeFlags) -> io::Result<(OwnedFd, OwnedFd)> {
     backend::io::syscalls::pipe_with(flags)
+}
+
+/// splice
+#[cfg(any(target_os = "android", target_os = "linux"))]
+#[inline]
+pub fn splice<FdIn: AsFd, FdOut: AsFd>(
+    fd_in: FdIn,
+    off_in: Option<u64>,
+    fd_out: FdOut,
+    off_out: Option<u64>,
+    len: usize,
+    flags: SpliceFlags,
+) -> io::Result<usize> {
+    backend::io::syscalls::splice(fd_in.as_fd(), off_in, fd_out.as_fd(), off_out, len, flags)
 }
