7b1f39690267f454e37ce161564e8a02635f61e2
Dan Gohman <dev@sunfishcode.online>
2022-10-10 11:55:43 -0700

    Add a `proc_self_status` function for reading /proc/self/status on Linux.
    Fixes #423.

--- tests/process/proc.rs
+++ tests/process/proc.rs
@@ -1,5 +1,6 @@
 #[test]
 fn test_proc_funcs() {
     let _maps = rustix::io::proc_self_maps().unwrap();
+    let _status = rustix::io::proc_self_status().unwrap();
     let _pagemap = rustix::io::proc_self_pagemap().unwrap();
 }
