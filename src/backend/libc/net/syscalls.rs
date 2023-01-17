c24be3e8eccefa8329f1f17e5b5a86a3a4bfc6f4
Dan Gohman <dev@sunfishcode.online>
2023-01-17 11:54:56 -0800

    Fix compilation on armv7-unknown-freebsd (#518)
    * Fix compilation on armv7-unknown-freebsd
    
    Use `time_t::MAX` to avoid depending on the relationship between
    `time_t` and `c_long`.
    
    * Silence the deprecation warning about `time_t`.

--- src/backend/libc/net/syscalls.rs
+++ src/backend/libc/net/syscalls.rs
@@ -555,11 +555,12 @@ pub(crate) mod sockopt {
                     return Err(io::Errno::INVAL);
                 }
 
-                let tv_sec = timeout.as_secs().try_into();
-                #[cfg(not(all(target_arch = "x86_64", target_pointer_width = "32")))]
-                let tv_sec = tv_sec.unwrap_or(c::c_long::MAX);
-                #[cfg(all(target_arch = "x86_64", target_pointer_width = "32"))]
-                let tv_sec = tv_sec.unwrap_or(i64::MAX);
+                // Rust's musl libc bindings deprecated `time_t` while they
+                // transition to 64-bit `time_t`. What we want here is just
+                // "whatever type `timeval`'s `tv_sec` is", so we're ok using
+                // the deprecated type.
+                #[allow(deprecated)]
+                let tv_sec = timeout.as_secs().try_into().unwrap_or(c::time_t::MAX);
 
                 // `subsec_micros` rounds down, so we use `subsec_nanos` and
                 // manually round up.
