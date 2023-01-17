7d4f5f479a6f4ff316da374a8ff565797ec7fdf2
Dan Gohman <dev@sunfishcode.online>
2023-01-17 10:01:28 -0800

    Silence the deprecation warning about `time_t`.

--- src/backend/libc/net/syscalls.rs
+++ src/backend/libc/net/syscalls.rs
@@ -555,6 +555,11 @@ pub(crate) mod sockopt {
                     return Err(io::Errno::INVAL);
                 }
 
+                // Rust's musl libc bindings deprecated `time_t` while they
+                // transition to 64-bit `time_t`. What we want here is just
+                // "whatever type `timeval`'s `tv_sec` is", so we're ok using
+                // the deprecated type.
+                #[allow(deprecated)]
                 let tv_sec = timeout.as_secs().try_into().unwrap_or(c::time_t::MAX);
 
                 // `subsec_micros` rounds down, so we use `subsec_nanos` and
