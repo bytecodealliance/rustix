e618560f2007df6d7847db9925590aff1c37ad54
Dan Gohman <dev@sunfishcode.online>
2023-03-03 22:08:19 -0800

    Use `weak!` for `setns`, `preadv64v2`, and `pwritev64v2` on GLIBC.
    `setns`, `preadv64v2`, and `pwritev64v2` were introduced more recently
    than the earliest glibc we support, so use the `weak!` mechanism for
    them, with fallbacks using `syscall`.

--- src/backend/libc/thread/syscalls.rs
+++ src/backend/libc/thread/syscalls.rs
@@ -287,6 +287,12 @@ pub(crate) fn gettid() -> Pid {
 #[cfg(any(target_os = "android", target_os = "linux"))]
 #[inline]
 pub(crate) fn setns(fd: BorrowedFd, nstype: c::c_int) -> io::Result<c::c_int> {
+    // `setns` wasn't supported in glibc until 2.14, and musl until 0.9.5,
+    // so use `syscall`.
+    weak_or_syscall! {
+        fn setns(fd: c::c_int, nstype: c::c_int) via SYS_setns -> c::c_int
+    }
+
     unsafe { ret_c_int(c::setns(borrowed_fd(fd), nstype)) }
 }
 
