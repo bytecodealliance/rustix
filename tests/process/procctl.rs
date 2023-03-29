a5e1f482a0906ad708df20fca6196e5fcfc33d8d
Val Packett <val@packett.cool>
2023-03-29 18:33:54 -0300

    Add more procctls for FreeBSD (#581)
    * Add PROC_REAP_* to the FreeBSD procctl module
    
    * Add PROC_TRAPCAP_* to the FreeBSD procctl module
    
    * Add PROC_NO_NEW_PRIVS_* to the FreeBSD procctl module

--- tests/process/procctl.rs
+++ tests/process/procctl.rs
@@ -1,3 +1,4 @@
+use rustix::io;
 use rustix::process::*;
 
 #[test]
@@ -8,4 +9,37 @@ fn test_parent_process_death_signal() {
 #[test]
 fn test_trace_status() {
     dbg!(trace_status(None).unwrap());
+}
+
+#[test]
+fn test_reaper_status() {
+    assert_eq!(set_reaper_status(false), Err(io::Errno::INVAL));
+    set_reaper_status(true).unwrap();
+    let status_while_acq = dbg!(get_reaper_status(None).unwrap());
+    set_reaper_status(false).unwrap();
+    let status_while_rel = dbg!(get_reaper_status(None).unwrap());
+    assert!(status_while_acq.flags.contains(ReaperStatusFlags::OWNED));
+    assert!(!status_while_rel.flags.contains(ReaperStatusFlags::OWNED));
+}
+
+#[test]
+fn test_reaper_pids() {
+    dbg!(get_reaper_pids(None).unwrap());
+}
+
+#[test]
+fn test_trapcap() {
+    assert_eq!(trap_cap_behavior(None).unwrap(), TrapCapBehavior::Disable);
+    set_trap_cap_behavior(None, TrapCapBehavior::Enable).unwrap();
+    assert_eq!(trap_cap_behavior(None).unwrap(), TrapCapBehavior::Enable);
+    set_trap_cap_behavior(None, TrapCapBehavior::Disable).unwrap();
+    assert_eq!(trap_cap_behavior(None).unwrap(), TrapCapBehavior::Disable);
+}
+
+#[test]
+fn test_no_new_privs() {
+    assert!(!no_new_privs(None).unwrap());
+    set_no_new_privs(None).unwrap();
+    assert!(no_new_privs(None).unwrap());
+    // No going back but, well, we're not gonna execute SUID binaries from the test suite.
 }
