945b7efe01e1e6acc77e11bac005fb82cbb47dd7
Dan Gohman <dev@sunfishcode.online>
2023-05-24 09:54:21 -0700

    Add non-at filesystem functions.

--- tests/fs/openat.rs
+++ tests/fs/openat.rs
@@ -1,7 +1,26 @@
 use std::fs::File;
 
-use rustix::fs::{cwd, openat, Mode, OFlags};
+use rustix::fs::{cwd, open, openat, Mode, OFlags};
 use std::io::Write;
+
+#[test]
+fn test_open_tmpfile() {
+    let tmp = tempfile::tempdir().unwrap();
+    let f = match open(
+        tmp.path(),
+        OFlags::WRONLY | OFlags::CLOEXEC | OFlags::TMPFILE,
+        Mode::from_bits_truncate(0o644),
+    ) {
+        Ok(f) => Ok(Some(File::from(f))),
+        Err(rustix::io::Errno::OPNOTSUPP | rustix::io::Errno::ISDIR | rustix::io::Errno::NOENT) => {
+            Ok(None)
+        }
+        Err(err) => Err(err),
+    };
+    if let Some(mut f) = f.unwrap() {
+        write!(f, "hello world").unwrap();
+    }
+}
 
 #[test]
 fn test_openat_tmpfile() {
