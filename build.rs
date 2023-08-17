bytecodealliance/rustix

commit b15b2792de13bd5030e09ca9fe9b0619146a1ed6

Move experimental Rust features behind a `--cfg=`. (#754)
Instead of just autodetecting nightly-only Rust features, put them behind
a `--cfg` so that they're not feature-detected for users.

build.rs
@@ -9,17 +9,6 @@ fn main() {
     // the rustc version.
     println!("cargo:rerun-if-changed=build.rs");
 
-    use_feature_or_nothing("rustc_attrs");
-
-    // Features only used in no-std configurations.
-    #[cfg(not(feature = "std"))]
-    {
-        use_feature_or_nothing("core_c_str");
-        use_feature_or_nothing("core_ffi_c");
-        use_feature_or_nothing("alloc_c_string");
-        use_feature_or_nothing("alloc_ffi");
-    }
-
     // Gather target information.
     let arch = var("CARGO_CFG_TARGET_ARCH").unwrap();
     let env = var("CARGO_CFG_TARGET_ENV").unwrap();
@@ -45,6 +34,13 @@ fn main() {
     // enable the libc backend even if rustix is depended on transitively.
     let cfg_use_libc = var("CARGO_CFG_RUSTIX_USE_LIBC").is_ok();
 
+    // Check for eg. `RUSTFLAGS=--cfg=rustix_use_experimental_features`. This
+    // is a rustc flag rather than a cargo feature flag because it's
+    // experimental and not something we want accidentally enabled via
+    // `--all-features`.
+    let rustix_use_experimental_features =
+        var("CARGO_CFG_RUSTIX_USE_EXPERIMENTAL_FEATURES").is_ok();
+
     // Check for eg. `RUSTFLAGS=--cfg=rustix_use_experimental_asm`. This is a
     // rustc flag rather than a cargo feature flag because it's experimental
     // and not something we want accidentally enabled via `--all-features`.
@@ -53,6 +49,27 @@ fn main() {
     // Miri doesn't support inline asm, and has builtin support for recognizing
     // libc FFI calls, so if we're running under miri, use the libc backend.
     let miri = var("CARGO_CFG_MIRI").is_ok();
+
+    // If experimental features are enabled, auto-detect and use available
+    // features.
+    if rustix_use_experimental_features {
+        use_feature_or_nothing("rustc_attrs");
+        use_feature_or_nothing("core_intrinsics");
+    }
+
+    // Features needed only in no-std configurations.
+    #[cfg(not(feature = "std"))]
+    {
+        use_feature_or_nothing("core_c_str");
+        use_feature_or_nothing("core_ffi_c");
+        use_feature_or_nothing("alloc_c_string");
+        use_feature_or_nothing("alloc_ffi");
+    }
+
+    // Feature needed for testing.
+    if use_static_assertions() {
+        use_feature("static_assertions");
+    }
 
     // If the libc backend is requested, or if we're not on a platform for
     // which we have linux_raw support, use the libc backend.
@@ -74,7 +91,6 @@ fn main() {
     } else {
         // Use the linux_raw backend.
         use_feature("linux_raw");
-        use_feature_or_nothing("core_intrinsics");
         if rustix_use_experimental_asm {
             use_feature("asm_experimental_arch");
         }
@@ -131,14 +147,6 @@ fn main() {
             || (env == "musl" && arch == "x86"))
     {
         use_feature("fix_y2038");
-    }
-
-    if os == "wasi" {
-        use_feature_or_nothing("wasi_ext");
-    }
-
-    if use_static_assertions() {
-        use_feature("static_assertions");
     }
 
     println!("cargo:rerun-if-env-changed=CARGO_CFG_RUSTIX_USE_EXPERIMENTAL_ASM");

.github/workflows/main.yml
@@ -221,6 +221,8 @@ jobs:
     runs-on: ${{ matrix.os }}
     env:
       QEMU_BUILD_VERSION: 8.0.2
+      # Enabling testing of experimental features.
+      RUSTFLAGS: --cfg rustix_use_experimental_features
     strategy:
       matrix:
         build: [ubuntu, ubuntu-20.04, i686-linux, aarch64-linux, powerpc64le-linux, riscv64-linux, s390x-linux, arm-linux, ubuntu-stable, ubuntu-1.63, i686-linux-stable, aarch64-linux-stable, riscv64-linux-stable, s390x-linux-stable, mipsel-linux-stable, mips64el-linux-stable, powerpc64le-linux-stable, arm-linux-stable, ubuntu-1.63, i686-linux-1.63, aarch64-linux-1.63, riscv64-linux-1.63, s390x-linux-1.63, mipsel-linux-1.63, mips64el-linux-1.63, powerpc64le-linux-1.63, arm-linux-1.63, macos-latest, macos-11, windows, windows-2019]

commit a605c56bf107a737b0cd25276ea4dbaacae03fd3

Reintroduce the build-time check for wasi_ext on wasi targets.
Reintroduce the build-time check for wasi_ext, when the target os is
WASI. This fixes a regression introduced in #754.

build.rs
@@ -71,6 +71,11 @@ fn main() {
         use_feature("static_assertions");
     }
 
+    // WASI support can utilize wasi_ext if present.
+    if os == "wasi" {
+        use_feature_or_nothing("wasi_ext");
+    }
+
     // If the libc backend is requested, or if we're not on a platform for
     // which we have linux_raw support, use the libc backend.
     //