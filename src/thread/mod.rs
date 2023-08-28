4de4489560b4f1e12d38a13552a22bfda0ebc8e3
Dan Gohman <dev@sunfishcode.online>
2023-08-28 10:51:06 -0700

    Export `RawPid`/`RawUid`/`RawGid` types from `rustix::thread`.
    These accompany `Pid`/`Uid`/`Gid`.
    
    Also, export `Signal` from `rustix::runtime`.

--- src/thread/mod.rs
+++ src/thread/mod.rs
@@ -19,7 +19,8 @@ pub use clock::*;
 pub use futex::{futex, FutexFlags, FutexOperation};
 #[cfg(linux_kernel)]
 pub use id::{
-    gettid, set_thread_gid, set_thread_res_gid, set_thread_res_uid, set_thread_uid, Gid, Pid, Uid,
+    gettid, set_thread_gid, set_thread_res_gid, set_thread_res_uid, set_thread_uid, Gid, Pid,
+    RawGid, RawPid, RawUid, Uid,
 };
 #[cfg(linux_kernel)]
 pub use libcap::{capabilities, set_capabilities, CapabilityFlags, CapabilitySets};
