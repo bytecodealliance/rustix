3fb04eea22456902a285f3cbcad21a361b2b7615
Dan Gohman <dev@sunfishcode.online>
2023-09-10 05:24:16 -0700

    Work around a Rust 1.69 doc bug. (#819)
    Don't add documentation to public type aliases `Errno` and `GetRandomFlags`;
    just document the underlying types instead. This avoids a compiler abort in
    Rust 1.69.
    
    Fixes #818.

--- src/backend/libc/io/errno.rs
+++ src/backend/libc/io/errno.rs
@@ -6,10 +6,32 @@
 use crate::backend::c;
 use libc_errno::errno;
 
-/// The error type for `rustix` APIs.
+/// `errno`—An error code.
 ///
-/// This is similar to `std::io::Error`, but only holds an OS error code,
-/// and no extra error value.
+/// The error type for `rustix` APIs. This is similar to [`std::io::Error`],
+//// but only holds an OS error code, and no extra error value.
+///
+/// # References
+///  - [POSIX]
+///  - [Linux]
+///  - [Winsock2]
+///  - [FreeBSD]
+///  - [NetBSD]
+///  - [OpenBSD]
+///  - [DragonFly BSD]
+///  - [illumos]
+///  - [glibc]
+///
+/// [POSIX]: https://pubs.opengroup.org/onlinepubs/9699919799/functions/errno.html
+/// [Linux]: https://man7.org/linux/man-pages/man3/errno.3.html
+/// [Winsock2]: https://learn.microsoft.com/en-us/windows/win32/winsock/windows-sockets-error-codes-2
+/// [FreeBSD]: https://man.freebsd.org/cgi/man.cgi?errno
+/// [NetBSD]: https://man.netbsd.org/errno.2
+/// [OpenBSD]: https://man.openbsd.org/errno.2
+/// [DragonFly BSD]: https://man.dragonflybsd.org/?command=errno&section=2
+/// [illumos]: https://illumos.org/man/3C/errno
+/// [glibc]: https://www.gnu.org/software/libc/manual/html_node/Error-Codes.html
+/// [`std::io::Error`]: Result
 #[repr(transparent)]
 #[doc(alias = "errno")]
 #[derive(Eq, PartialEq, Hash, Copy, Clone)]
