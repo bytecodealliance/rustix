c2c6450ac8f01c2cb9895ba04bacda30738bd0d3
Dan Gohman <dev@sunfishcode.online>
2023-09-28 22:22:17 -0700

    Fix compilation on use-libc.

--- src/backend/libc/fs/syscalls.rs
+++ src/backend/libc/fs/syscalls.rs
@@ -258,7 +258,7 @@ pub(crate) fn readlink(path: &CStr, buf: &mut [u8]) -> io::Result<usize> {
     }
 }
 
-#[cfg(all(feature = "alloc", not(target_os = "redox")))]
+#[cfg(all(any(feature = "alloc", feature = "procfs"), not(target_os = "redox")))]
 #[inline]
 pub(crate) fn readlinkat(
     dirfd: BorrowedFd<'_>,
