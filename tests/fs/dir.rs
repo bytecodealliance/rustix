162052a84afce307c38a6243775f43a4f9401cbb
Dan Gohman <dev@sunfishcode.online>
2023-10-12 10:19:59 -0700

    Fixes for `Dir` on macOS, FreeBSD, and WASI.

--- tests/fs/dir.rs
+++ tests/fs/dir.rs
@@ -62,6 +62,10 @@ fn test_dir() {
     assert!(saw_cargo_toml);
 }
 
+// Test that `Dir` silently stops iterating if the directory has been removed.
+//
+// Except on FreeBSD and macOS, where apparently `readdir` just keeps reading.
+#[cfg_attr(any(apple, freebsdlike), ignore)]
 #[test]
 fn dir_iterator_handles_dir_removal() {
     // create a dir, keep the FD, then delete the dir
@@ -83,6 +87,7 @@ fn dir_iterator_handles_dir_removal() {
 
 // Like `dir_iterator_handles_dir_removal`, but close the directory after
 // `Dir::read_from`.
+#[cfg_attr(any(apple, freebsdlike), ignore)]
 #[test]
 fn dir_iterator_handles_dir_removal_after_open() {
     // create a dir, keep the FD, then delete the dir
