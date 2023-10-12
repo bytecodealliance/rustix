31fd98ca723b93cc6101a3e29843ea5cf094e159
Dan Gohman <dev@sunfishcode.online>
2023-10-12 08:13:24 -0700

    Merge pull request from GHSA-c827-hfw6-qwvm
    * Fix `rustix::fs::Dir` to avoid unbounded buffer growth.
    
    Fix `Dir`'s buffer size computation to avoid resizing past a fixed
    upper limit. This prevents it from growing without bound, such as in
    the case of `Dir::rewind` for repeated iterations with the same `Dir`.
    
    * Don't let `Dir` continue to try to iterate after a failure.
    
    * Handle `io::Errno::INTR` gracefully.
    
    * Write a more detailed comment on the buffer growth policy.
    
    * Also mention that no buffer can ever be big enough for everything.
    
    * Add tests against over-allocation & stuck iterator
    
    * Rm `dir_iterator_does_not_overallocate` unit test in favour of docs
    
    * Extend `test_dir` to cover `rewind`.
    
    * Consistently handle directory removal as ending the stream.
    
    libc implementations of directory iteration handle directory removal
    by just ending the stream. In the linux_raw backend, this looks like
    `ENOENT` from `getdents64`, so change the code to check for `ENOENT`
    and end the stream.
    
    This requires changing the `dir_iterator_does_not_get_stuck_on_io_error`
    test to no longer expect a failure, so it's now renamed to
    `dir_iterator_handles_dir_removal`.
    
    To test the error case, add a new `dir_iterator_handles_io_errors`
    test which uses `dup2` to induce an error, in both the linux_raw and
    libc backends.
    
    This exposes the fact that the libc `Dir` implementation was also
    assuming that users would stop iterating after hitting a failure, so
    add a `any_errors` flag to the libc backend as well.
    
    * Add a test for removing the directory after doing `read_from`.
    
    * In the libc backend, handle `ENOENT` when opening ".".
    
    ---------
    
    Co-authored-by: cyqsimon <28627918+cyqsimon@users.noreply.github.com>

--- tests/fs/dir.rs
+++ tests/fs/dir.rs
@@ -8,7 +8,7 @@ fn test_dir() {
     )
     .unwrap();
 
-    let dir = rustix::fs::Dir::read_from(&t).unwrap();
+    let mut dir = rustix::fs::Dir::read_from(&t).unwrap();
 
     let _file = rustix::fs::openat(
         &t,
@@ -18,6 +18,32 @@ fn test_dir() {
     )
     .unwrap();
 
+    // Read the directory entries. We use `while let Some(entry)` so that we
+    // don't consume the `Dir` so that we can run more tests on it.
+    let mut saw_dot = false;
+    let mut saw_dotdot = false;
+    let mut saw_cargo_toml = false;
+    while let Some(entry) = dir.read() {
+        let entry = entry.unwrap();
+        if entry.file_name() == rustix::cstr!(".") {
+            saw_dot = true;
+        } else if entry.file_name() == rustix::cstr!("..") {
+            saw_dotdot = true;
+        } else if entry.file_name() == rustix::cstr!("Cargo.toml") {
+            saw_cargo_toml = true;
+        }
+    }
+    assert!(saw_dot);
+    assert!(saw_dotdot);
+    assert!(saw_cargo_toml);
+
+    // Rewind the directory so we can iterate over the entries again.
+    dir.rewind();
+
+    // For what comes next, we don't need `mut` anymore.
+    let dir = dir;
+
+    // Read the directory entries, again. This time we use `for entry in dir`.
     let mut saw_dot = false;
     let mut saw_dotdot = false;
     let mut saw_cargo_toml = false;
@@ -34,4 +60,45 @@ fn test_dir() {
     assert!(saw_dot);
     assert!(saw_dotdot);
     assert!(saw_cargo_toml);
+}
+
+#[test]
+fn dir_iterator_handles_dir_removal() {
+    // create a dir, keep the FD, then delete the dir
+    let tmp = tempfile::tempdir().unwrap();
+    let fd = rustix::fs::openat(
+        rustix::fs::CWD,
+        tmp.path(),
+        rustix::fs::OFlags::RDONLY | rustix::fs::OFlags::CLOEXEC,
+        rustix::fs::Mode::empty(),
+    )
+    .unwrap();
+
+    // Drop the `TempDir`, which deletes the directory.
+    drop(tmp);
+
+    let mut dir = rustix::fs::Dir::read_from(&fd).unwrap();
+    assert!(matches!(dir.next(), None));
+}
+
+// Like `dir_iterator_handles_dir_removal`, but close the directory after
+// `Dir::read_from`.
+#[test]
+fn dir_iterator_handles_dir_removal_after_open() {
+    // create a dir, keep the FD, then delete the dir
+    let tmp = tempfile::tempdir().unwrap();
+    let fd = rustix::fs::openat(
+        rustix::fs::CWD,
+        tmp.path(),
+        rustix::fs::OFlags::RDONLY | rustix::fs::OFlags::CLOEXEC,
+        rustix::fs::Mode::empty(),
+    )
+    .unwrap();
+
+    let mut dir = rustix::fs::Dir::read_from(&fd).unwrap();
+
+    // Drop the `TempDir`, which deletes the directory.
+    drop(tmp);
+
+    assert!(matches!(dir.next(), None));
 }
