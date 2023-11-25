e85eae8977a5abf60215a7d59790a2df8b2f91b8
John Nunley <dev@notgull.net>
2023-11-24 18:27:16 -0800

    Uninitialized slices for getrandom
    Signed-off-by: John Nunley <dev@notgull.net>

--- src/rand/getrandom.rs
+++ src/rand/getrandom.rs
@@ -1,4 +1,8 @@
+#![allow(unsafe_code)]
+
 use crate::{backend, io};
+use core::mem::MaybeUninit;
+use core::slice;
 
 pub use backend::rand::types::GetRandomFlags;
 
@@ -16,5 +20,36 @@ pub use backend::rand::types::GetRandomFlags;
 /// [Linux]: https://man7.org/linux/man-pages/man2/getrandom.2.html
 #[inline]
 pub fn getrandom(buf: &mut [u8], flags: GetRandomFlags) -> io::Result<usize> {
-    backend::rand::syscalls::getrandom(buf, flags)
+    unsafe { backend::rand::syscalls::getrandom(buf.as_mut_ptr(), buf.len(), flags) }
+}
+
+/// `getrandom(buf, flags)`—Reads a sequence of random bytes.
+///
+/// This is a very low-level API which may be difficult to use correctly. Most
+/// users should prefer to use [`getrandom`] or [`rand`] APIs instead.
+///
+/// This is identical to [`getn`]
+///
+/// [`getrandom`]: https://crates.io/crates/getrandom
+/// [`rand`]: https://crates.io/crates/rand
+///
+/// # References
+///  - [Linux]
+///
+/// [Linux]: https://man7.org/linux/man-pages/man2/getrandom.2.html
+#[inline]
+pub fn getrandom_uninit(
+    buf: &mut [MaybeUninit<u8>],
+    flags: GetRandomFlags,
+) -> io::Result<(&mut [u8], &mut [MaybeUninit<u8>])> {
+    // Get number of initialized bytes.
+    let length = unsafe {
+        backend::rand::syscalls::getrandom(buf.as_mut_ptr() as *mut u8, buf.len(), flags)
+    };
+
+    // Split into the initialized and uninitialized portions.
+    let (init, uninit) = buf.split_at_mut(length?);
+    let init = unsafe { slice::from_raw_parts_mut(init.as_mut_ptr() as *mut u8, init.len()) };
+
+    Ok((init, uninit))
 }
--- tests/rand/getrandom.rs
+++ tests/rand/getrandom.rs
@@ -1,7 +1,16 @@
-use rustix::rand::{getrandom, GetRandomFlags};
+use core::mem::MaybeUninit;
+use rustix::rand::{getrandom, getrandom_uninit, GetRandomFlags};
 
 #[test]
 fn test_getrandom() {
     let mut buf = [0_u8; 256];
     let _ = getrandom(&mut buf, GetRandomFlags::empty());
+}
+
+#[test]
+fn test_getrandom_uninit() {
+    let mut buf = unsafe { MaybeUninit::<[MaybeUninit<u8>; 256]>::uninit().assume_init() };
+    let (init, uninit) = getrandom_uninit(&mut buf, GetRandomFlags::empty()).unwrap();
+    let combined_len = init.len() + uninit.len();
+    assert_eq!(buf.len(), combined_len);
 }
