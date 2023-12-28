46ab1f5b0942c48dca28fe82c362500d9b393991
rusty-snake <41237666+rusty-snake@users.noreply.github.com>
2023-12-28 16:26:00 +0100

    Add NOEXEC_SEAL and EXEC to MemfdFlags
    Enabling non-executable memfds: https://lwn.net/Articles/918106/

--- src/backend/linux_raw/fs/types.rs
+++ src/backend/linux_raw/fs/types.rs
@@ -428,6 +428,11 @@ bitflags! {
         /// `MFD_HUGETLB` (since Linux 4.14)
         const HUGETLB = linux_raw_sys::general::MFD_HUGETLB;
 
+        /// `MFD_NOEXEC_SEAL` (since Linux 6.3)
+        const NOEXEC_SEAL = linux_raw_sys::general::MFD_NOEXEC_SEAL;
+        /// `MFD_EXEC` (since Linux 6.3)
+        const EXEC = linux_raw_sys::general::MFD_EXEC;
+
         /// `MFD_HUGE_64KB`
         const HUGE_64KB = linux_raw_sys::general::MFD_HUGE_64KB;
         /// `MFD_HUGE_512JB`
