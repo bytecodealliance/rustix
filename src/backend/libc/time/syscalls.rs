01561efbaf49610a698c8c527a2062c11f4f5f5a
Dan Gohman <dev@sunfishcode.online>
2024-01-11 08:17:59 -0800

    Fix breakage caused by `CLOCK_BOOTTIME` being removed from DragonFly.
    rust-lang/libc#3509 removed `CLOCK_BOOTIME` from DragonFly, which broke
    rustix.

--- src/backend/libc/time/syscalls.rs
+++ src/backend/libc/time/syscalls.rs
@@ -167,7 +167,7 @@ pub(crate) fn clock_gettime_dynamic(id: DynamicClockId<'_>) -> io::Result<Timesp
         DynamicClockId::Tai => c::CLOCK_TAI,
 
         #[cfg(any(
-            freebsdlike,
+            target_os = "freebsd",
             linux_kernel,
             target_os = "fuchsia",
             target_os = "openbsd"
