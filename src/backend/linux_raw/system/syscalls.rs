30739befde0dd499f64c7385441210fd26ceb956
LoveSy <shana@zju.edu.cn>
2024-02-16 04:26:17 +0800

    Add `init_module`, `finit_module`, and `delete_module` syscalls (#1005)

--- src/backend/linux_raw/system/syscalls.rs
+++ src/backend/linux_raw/system/syscalls.rs
@@ -7,7 +7,10 @@
 
 use super::types::RawUname;
 use crate::backend::c;
-use crate::backend::conv::{c_int, ret, ret_infallible, slice};
+use crate::backend::conv::{c_int, ret, ret_error, ret_infallible, slice};
+#[cfg(feature = "fs")]
+use crate::fd::BorrowedFd;
+use crate::ffi::CStr;
 use crate::io;
 use crate::system::{RebootCommand, Sysinfo};
 use core::mem::MaybeUninit;
@@ -46,4 +49,39 @@ pub(crate) fn reboot(cmd: RebootCommand) -> io::Result<()> {
             c_int(cmd as i32)
         ))
     }
+}
+
+#[cfg(any(target_os = "linux", target_os = "android"))]
+#[inline]
+pub(crate) fn init_module(image: &[u8], param_values: &CStr) -> io::Errno {
+    let (image, len) = slice(image);
+    unsafe {
+        ret_error(syscall_readonly!(
+            __NR_init_module,
+            image,
+            len,
+            param_values
+        ))
+    }
+}
+
+#[cfg(any(target_os = "linux", target_os = "android"))]
+#[cfg(feature = "fs")]
+#[inline]
+pub(crate) fn finit_module(fd: BorrowedFd<'_>, param_values: &CStr, flags: c::c_int) -> io::Errno {
+    unsafe {
+        ret_error(syscall_readonly!(
+            __NR_finit_module,
+            fd,
+            param_values,
+            c_int(flags)
+        ))
+    }
+}
+
+#[cfg(any(target_os = "linux", target_os = "android"))]
+#[cfg(feature = "fs")]
+#[inline]
+pub(crate) fn delete_module(name: &CStr, flags: c::c_int) -> io::Errno {
+    unsafe { ret_error(syscall_readonly!(__NR_delete_module, name, c_int(flags))) }
 }
