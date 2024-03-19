4b7e0a08405ceb367ab190a9c7a9246fb295c772
Dan Gohman <dev@sunfishcode.online>
2024-03-19 12:52:35 -0700

    Add documentation for `mount2`.

--- src/mount/mount_unmount.rs
+++ src/mount/mount_unmount.rs
@@ -43,6 +43,10 @@ pub fn mount<Source: path::Arg, Target: path::Arg, Fs: path::Arg, Data: path::Ar
 
 /// `mount2(source, target, filesystemtype, mountflags, data)`
 ///
+/// This is same as the [`mount`], except it adds support for
+/// the source, target, and data being omitted, and the data
+/// is passed as a `CStr` rather than a `path::Arg`.
+///
 /// # References
 ///  - [Linux]
 ///
