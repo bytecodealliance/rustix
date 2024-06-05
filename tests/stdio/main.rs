67f0706823e4d0d0e49391f28dc129e6df77900b
Dan Gohman <dev@sunfishcode.online>
2024-06-05 11:46:03 -0400

    rustfmt and fix test compilation.

--- tests/stdio/main.rs
+++ tests/stdio/main.rs
@@ -1,10 +1,12 @@
 //! Tests for [`rustix::stdio`].
 
-#[cfg(not(feature = "rustc-dep-of-std"))]
-#[cfg(not(windows))]
-#[cfg(not(target_os = "wasi"))]
-mod dup2_to_replace_stdio;
+#![cfg(feature = "stdio")]
+
 #[cfg(not(feature = "rustc-dep-of-std"))]
 #[cfg(not(windows))]
 #[cfg(not(target_os = "wasi"))]
 mod dup2_stdio;
+#[cfg(not(feature = "rustc-dep-of-std"))]
+#[cfg(not(windows))]
+#[cfg(not(target_os = "wasi"))]
+mod dup2_to_replace_stdio;
