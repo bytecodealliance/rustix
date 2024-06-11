cb2c70d9b32d31a3ecad92a7c8240c72d24207c4
Dan Gohman <dev@sunfishcode.online>
2024-06-11 09:47:58 -0700

    Fix `dup2_stdout` et al to handle fd aliasing. (#1069)
    * Fix `dup2_stdout` et al to handle fd aliasing.
    
    Fix `dup2_stdout` et al to work in the case where the file descriptor
    they are passed is the same as the file descriptor they implicitly
    operate on, including on targets that don't have a `dup2` syscall and
    implement it using `dup3`, such as aarch64.
    
    Fixes #1067.
    
    * rustfmt and fix test compilation.

--- tests/stdio/main.rs
+++ tests/stdio/main.rs
@@ -1,5 +1,11 @@
 //! Tests for [`rustix::stdio`].
 
+#![cfg(feature = "stdio")]
+
+#[cfg(not(feature = "rustc-dep-of-std"))]
+#[cfg(not(windows))]
+#[cfg(not(target_os = "wasi"))]
+mod dup2_stdio;
 #[cfg(not(feature = "rustc-dep-of-std"))]
 #[cfg(not(windows))]
 #[cfg(not(target_os = "wasi"))]
