a01a716fbe6d85dcb2954c71f101e7989ed7ec54
primoly <168267431+primoly@users.noreply.github.com>
2024-08-26 15:51:35 +0200

    Do some todos (#1092)
    * Do some todos
    
    * Add a `MOVE_MOUNT_BENEATH` constant to linux_raw as a temporary workaround.
    
    * Document that `MOVE_MOUNT_BENEATH` is since Linux 6.5.
    
    ---------
    
    Co-authored-by: Dan Gohman <dev@sunfishcode.online>

--- src/backend/linux_raw/c.rs
+++ src/backend/linux_raw/c.rs
@@ -320,3 +320,6 @@ pub(crate) use reboot_symbols::*;
 
 // TODO: This is new in Linux 6.11; remove when linux-raw-sys is updated.
 pub(crate) const MAP_DROPPABLE: u32 = 0x8;
+
+// TODO: This is new in Linux 6.5; remove when linux-raw-sys is updated.
+pub(crate) const MOVE_MOUNT_BENEATH: u32 = 0x200;
