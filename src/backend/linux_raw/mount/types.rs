f84be4ba89234492ee45a40fbed8b7eac612d1a1
Dan Gohman <dev@sunfishcode.online>
2024-08-26 06:38:45 -0700

    Add a `MOVE_MOUNT_BENEATH` constant to linux_raw as a temporary workaround.

--- src/backend/linux_raw/mount/types.rs
+++ src/backend/linux_raw/mount/types.rs
@@ -224,7 +224,7 @@ bitflags! {
         const MOVE_MOUNT_SET_GROUP = linux_raw_sys::general::MOVE_MOUNT_SET_GROUP;
 
         /// `MOVE_MOUNT_BENEATH`
-        const MOVE_MOUNT_BENEATH = linux_raw_sys::general::MOVE_MOUNT_BENEATH;
+        const MOVE_MOUNT_BENEATH = c::MOVE_MOUNT_BENEATH;
 
         /// `MOVE_MOUNT__MASK`
         const MOVE_MOUNT__MASK = linux_raw_sys::general::MOVE_MOUNT__MASK;
