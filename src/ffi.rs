4d0458c4e94abc9b10df2952569ade8d7ded6f26
Dan Gohman <dev@sunfishcode.online>
2024-09-05 09:04:53 -0700

    Add `ffi::c_void` on Windows.

--- src/ffi.rs
+++ src/ffi.rs
@@ -4,7 +4,7 @@
 #[cfg(windows)]
 #[cfg(feature = "std")]
 pub use std::os::raw::{
-    c_char, c_int, c_long, c_longlong, c_short, c_uint, c_ulong, c_ulonglong, c_ushort,
+    c_char, c_int, c_long, c_longlong, c_short, c_uint, c_ulong, c_ulonglong, c_ushort, c_void,
 };
 #[cfg(not(windows))]
 #[cfg(feature = "std")]
