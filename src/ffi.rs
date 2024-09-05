6b86e33af2fc9a1f8e425bf4fdb3308f31ee2aae
Dan Gohman <dev@sunfishcode.online>
2024-09-05 08:50:53 -0700

    Add `c_ulonglong`/`c_longlong` to the ffi module.

--- src/ffi.rs
+++ src/ffi.rs
@@ -3,12 +3,16 @@
 // If we have std, use it.
 #[cfg(windows)]
 #[cfg(feature = "std")]
-pub use std::os::raw::{c_char, c_int, c_long, c_short, c_uint, c_ulong, c_ushort};
+pub use std::os::raw::{
+    c_char, c_int, c_long, c_longlong, c_short, c_uint, c_ulong, c_ulonglong, c_ushort,
+};
 #[cfg(not(windows))]
 #[cfg(feature = "std")]
 pub use {
     std::ffi::{CStr, CString, FromBytesWithNulError, NulError},
-    std::os::raw::{c_char, c_int, c_long, c_short, c_uint, c_ulong, c_ushort, c_void},
+    std::os::raw::{
+        c_char, c_int, c_long, c_longlong, c_short, c_uint, c_ulong, c_ulonglong, c_ushort, c_void,
+    },
 };
 
 // If we don't have std, we can depend on core and alloc having these features
@@ -18,9 +22,12 @@ pub use {
 pub use alloc::ffi::{CString, NulError};
 #[cfg(windows)]
 #[cfg(not(feature = "std"))]
-pub use core::ffi::{c_char, c_int, c_long, c_short, c_uint, c_ulong, c_ushort, c_void};
+pub use core::ffi::{
+    c_char, c_int, c_long, c_longlong, c_short, c_uint, c_ulong, c_ulonglong, c_ushort, c_void,
+};
 #[cfg(not(windows))]
 #[cfg(not(feature = "std"))]
 pub use core::ffi::{
-    c_char, c_int, c_long, c_short, c_uint, c_ulong, c_ushort, c_void, CStr, FromBytesWithNulError,
+    c_char, c_int, c_long, c_longlong, c_short, c_uint, c_ulong, c_ulonglong, c_ushort, c_void,
+    CStr, FromBytesWithNulError,
 };
