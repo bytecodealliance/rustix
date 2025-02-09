0fb7cdd45d17ebf9c53c98bfbe1cce695cd60f8a
Dan Gohman <dev@sunfishcode.online>
2025-02-08 17:03:25 -0800

    Fix the `fadvise` handling of large offsets on FreeBSD.
    On FreeBSD, when a large `u64` converts to a negative offset, we add
    some special-case behavior to hide the fact that this conversion is
    happening. Clarify the comments, update the code to do a proper no-op
    in the case of a negative offset, and use the correct length in the
    case of an overflowing offset+length.

--- src/fs/fadvise.rs
+++ src/fs/fadvise.rs
@@ -8,9 +8,11 @@ use backend::fs::types::Advice;
 /// # References
 ///  - [POSIX]
 ///  - [Linux]
+///  - [FreeBSD]
 ///
 /// [POSIX]: https://pubs.opengroup.org/onlinepubs/9799919799/functions/posix_fadvise.html
 /// [Linux]: https://man7.org/linux/man-pages/man2/posix_fadvise.2.html
+/// [FreeBSD]: https://man.freebsd.org/cgi/man.cgi?query=posix_fadvise&sektion=2
 #[inline]
 #[doc(alias = "posix_fadvise")]
 pub fn fadvise<Fd: AsFd>(fd: Fd, offset: u64, len: u64, advice: Advice) -> io::Result<()> {
