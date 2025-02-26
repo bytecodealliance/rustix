847bceb2f9e32611979741105c64ca64ff362c25
Your Name <you@example.com>
2025-02-26 06:58:48 +0000

    Fix test_vdso to use __kernel_clock_gettime64

--- src/backend/linux_raw/vdso.rs
+++ src/backend/linux_raw/vdso.rs
@@ -486,8 +486,10 @@ fn test_vdso() {
         let ptr = vdso.sym(cstr!("LINUX_2.6"), cstr!("__vdso_gettimeofday"));
         #[cfg(target_arch = "riscv64")]
         let ptr = vdso.sym(cstr!("LINUX_4.15"), cstr!("__vdso_gettimeofday"));
-        #[cfg(any(target_arch = "powerpc", target_arch = "powerpc64"))]
-        let ptr = vdso.sym(cstr!("LINUX_2.6.15"), cstr!("__kernel_gettimeofday"));
+        #[cfg(target_arch = "powerpc")]
+        let ptr = vdso.sym(cstr!("LINUX_5.11"), cstr!("__kernel_clock_gettime64"));
+        #[cfg(target_arch = "powerpc64")]
+        let ptr = vdso.sym(cstr!("LINUX_2.6.15"), cstr!("__kernel_clock_gettime"));
         #[cfg(target_arch = "s390x")]
         let ptr = vdso.sym(cstr!("LINUX_2.6.29"), cstr!("__kernel_gettimeofday"));
         #[cfg(any(target_arch = "mips", target_arch = "mips32r6"))]
