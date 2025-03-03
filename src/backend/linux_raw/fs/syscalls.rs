0ae62267411756e0ee7f8547058be492d8d8b978
Dan Gohman <dev@sunfishcode.online>
2025-03-03 14:52:50 -0800

    Fix handling of negative timestamps in `stat` on Linux.
    Some Linux `stat64` structs are defined with `st_atime`, `st_mtime`, and
    `st_ctime` fields using unsigned types, even though they hold signed
    values. Convert them to signed before converting them to `i64`, so that
    they're signed-extended.
    
    This fixes test failures on old 32-bit Linux platforms which lack
    `statx`.

--- src/backend/linux_raw/fs/syscalls.rs
+++ src/backend/linux_raw/fs/syscalls.rs
@@ -763,17 +763,17 @@ fn stat_to_stat(s64: linux_raw_sys::general::stat64) -> io::Result<Stat> {
         st_size: s64.st_size.try_into().map_err(|_| io::Errno::OVERFLOW)?,
         st_blksize: s64.st_blksize.try_into().map_err(|_| io::Errno::OVERFLOW)?,
         st_blocks: s64.st_blocks.try_into().map_err(|_| io::Errno::OVERFLOW)?,
-        st_atime: i64::from(s64.st_atime),
+        st_atime: i64::from(s64.st_atime.as_signed()),
         st_atime_nsec: s64
             .st_atime_nsec
             .try_into()
             .map_err(|_| io::Errno::OVERFLOW)?,
-        st_mtime: i64::from(s64.st_mtime),
+        st_mtime: i64::from(s64.st_mtime.as_signed()),
         st_mtime_nsec: s64
             .st_mtime_nsec
             .try_into()
             .map_err(|_| io::Errno::OVERFLOW)?,
-        st_ctime: i64::from(s64.st_ctime),
+        st_ctime: i64::from(s64.st_ctime.as_signed()),
         st_ctime_nsec: s64
             .st_ctime_nsec
             .try_into()
@@ -795,17 +795,17 @@ fn stat_to_stat(s: linux_raw_sys::general::stat) -> io::Result<Stat> {
         st_size: s.st_size.try_into().map_err(|_| io::Errno::OVERFLOW)?,
         st_blksize: s.st_blksize.try_into().map_err(|_| io::Errno::OVERFLOW)?,
         st_blocks: s.st_blocks.try_into().map_err(|_| io::Errno::OVERFLOW)?,
-        st_atime: i64::from(s.st_atime),
+        st_atime: i64::from(s.st_atime.as_signed()),
         st_atime_nsec: s
             .st_atime_nsec
             .try_into()
             .map_err(|_| io::Errno::OVERFLOW)?,
-        st_mtime: i64::from(s.st_mtime),
+        st_mtime: i64::from(s.st_mtime.as_signed()),
         st_mtime_nsec: s
             .st_mtime_nsec
             .try_into()
             .map_err(|_| io::Errno::OVERFLOW)?,
-        st_ctime: i64::from(s.st_ctime),
+        st_ctime: i64::from(s.st_ctime.as_signed()),
         st_ctime_nsec: s
             .st_ctime_nsec
             .try_into()
@@ -1671,4 +1671,36 @@ fn test_sizes() {
 
     // Assert that `Timestamps` has the expected layout.
     assert_eq_size!([linux_raw_sys::general::__kernel_timespec; 2], Timestamps);
+}
+
+// Some linux_raw_sys structs have unsigned types for values which are
+// interpreted as signed. This defines a utility or casting to the
+// same-sized signed type.
+trait AsSigned {
+    type Signed;
+    fn as_signed(self) -> Self::Signed;
+}
+impl AsSigned for u32 {
+    type Signed = i32;
+    fn as_signed(self) -> Self::Signed {
+        self as _
+    }
+}
+impl AsSigned for i32 {
+    type Signed = i32;
+    fn as_signed(self) -> Self::Signed {
+        self
+    }
+}
+impl AsSigned for u64 {
+    type Signed = i64;
+    fn as_signed(self) -> Self::Signed {
+        self as _
+    }
+}
+impl AsSigned for i64 {
+    type Signed = i64;
+    fn as_signed(self) -> Self::Signed {
+        self
+    }
 }
