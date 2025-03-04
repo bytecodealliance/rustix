604bce988fcae5ac405a9ac2d536b5d25c52c210
Dan Gohman <dev@sunfishcode.online>
2025-03-03 22:56:51 -0800

    Fix `Timespec` layout on Windows to match `Nsecs`.

--- src/backend/libc/winsock_c.rs
+++ src/backend/libc/winsock_c.rs
@@ -62,6 +62,6 @@ pub(crate) use WinSock::{
 // in its public API. So define one, and we'll convert it internally.
 pub struct timespec {
     pub tv_sec: time_t,
-    pub tv_nsec: i64,
+    pub tv_nsec: ffi::c_long,
 }
 pub type time_t = i64;
