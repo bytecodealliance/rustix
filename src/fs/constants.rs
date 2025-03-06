1fb693ca6dec27fecb35dc343a3fd85bf1ce68a7
rusty-snake <41237666+rusty-snake@users.noreply.github.com>
2025-03-06 15:39:17 +0100

    Add convenient is_ functions to FileType

--- src/fs/constants.rs
+++ src/fs/constants.rs
@@ -5,6 +5,50 @@ use crate::backend;
 pub use crate::timespec::{Nsecs, Secs, Timespec};
 pub use backend::fs::types::*;
 
+impl FileType {
+    /// Returns `true` if this `FileType` is a regular file.
+    pub fn is_file(self) -> bool {
+        self == Self::RegularFile
+    }
+
+    /// Returns `true` if this `FileType` is a directory.
+    pub fn is_dir(self) -> bool {
+        self == Self::Directory
+    }
+
+    /// Returns `true` if this `FileType` is a symlink.
+    pub fn is_symlink(self) -> bool {
+        self == Self::Symlink
+    }
+
+    /// Returns `true` if this `FileType` is a fifo.
+    #[cfg(not(target_os = "wasi"))]
+    pub fn is_fifo(self) -> bool {
+        self == Self::Fifo
+    }
+
+    /// Returns `true` if this `FileType` is a socket.
+    #[cfg(not(target_os = "wasi"))]
+    pub fn is_socket(self) -> bool {
+        self == Self::Socket
+    }
+
+    /// Returns `true` if this `FileType` is a character device.
+    pub fn is_char_device(self) -> bool {
+        self == Self::CharacterDevice
+    }
+
+    /// Returns `true` if this `FileType` is a block device.
+    pub fn is_block_device(self) -> bool {
+        self == Self::BlockDevice
+    }
+
+    /// Returns `true` if this `FileType` is unknown.
+    pub fn is_unknown(self) -> bool {
+        self == Self::Unknown
+    }
+}
+
 #[cfg(test)]
 #[allow(unused_imports)]
 #[allow(unsafe_code)]
