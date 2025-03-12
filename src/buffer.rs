4449d6aa4646fb1a109664d917ffc93ccf711045
Josh Triplett <josh@joshtriplett.org>
2025-03-12 00:25:29 -0700

    Clarify documentation of `Buffer::parts_mut`: length is elements, not bytes

--- src/buffer.rs
+++ src/buffer.rs
@@ -293,6 +293,8 @@ mod private {
 
         /// Return a pointer and length for this buffer.
         ///
+        /// The length is the number of elements of type `T`, not a number of bytes.
+        ///
         /// It's tempting to have this return `&mut [MaybeUninit<T>]` instead,
         /// however that would require this function to be `unsafe`, because
         /// callers could use the `&mut [MaybeUninit<T>]` slice to set elements
