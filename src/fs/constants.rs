ffbe76047d0c657fe6948ed4d8ea75f3e9525ab6
Dan Gohman <dev@sunfishcode.online>
2025-04-01 06:37:15 -0700

    Mark 32-bit x86 freebsd as needing y2038 fixes.
    On 32-bit x86 freebsd, time_t is 32-bit, so enable y2038 fixes.

--- src/fs/constants.rs
+++ src/fs/constants.rs
@@ -79,11 +79,15 @@ mod tests {
         ))]
         assert_eq_size!(u16, linux_raw_sys::general::__kernel_mode_t);
 
-        // Ensure that seconds fields are 64-bit.
-        let some_stat: Stat = unsafe { core::mem::zeroed() };
-        assert_eq!(some_stat.st_atime, 0_i64);
-        assert_eq!(some_stat.st_mtime, 0_i64);
-        assert_eq!(some_stat.st_ctime, 0_i64);
+        // Ensure that seconds fields are 64-bit on non-y2038-bug platforms, and
+        // on Linux where we use statx.
+        #[cfg(any(linux_kernel, not(fix_y2038)))]
+        {
+            let some_stat: Stat = unsafe { core::mem::zeroed() };
+            assert_eq!(some_stat.st_atime, 0_i64);
+            assert_eq!(some_stat.st_mtime, 0_i64);
+            assert_eq!(some_stat.st_ctime, 0_i64);
+        }
 
         // Ensure that file offsets are 64-bit.
         assert_eq!(some_stat.st_size, 0_i64);
