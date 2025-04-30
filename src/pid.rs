bytecodealliance/rustix

commit 4626b696cef063639ab3b6fed15b91c26475df73

Document that negative pids aren't UB, but may cause unexpected behavior. (#1443)
As discussed in #1433, allow `Pid` values to be negative without
invoking UB, because they can occur on systems configured with seccomp-bpf
to make syscalls like `getpid` fail. However, also document that while
this doesn't invoke UB, it isn't useful.

src/pid.rs
@@ -27,7 +27,7 @@ impl Pid {
 
     /// Converts a `RawPid` into a `Pid`.
     ///
-    /// Returns `Some` for positive `RawPid`s. Otherwise, returns `None`.
+    /// Returns `Some` for positive values, and `None` for zero values.
     ///
     /// This is safe because a `Pid` is a number without any guarantees for the
     /// kernel. Non-child `Pid`s are always racy for any syscalls, but can only
@@ -35,9 +35,13 @@ impl Pid {
     /// non-child processes, please consider other mechanisms like [pidfd] on
     /// Linux.
     ///
+    /// Passing a negative number doesn't invoke undefined behavior, but it
+    /// may cause unexpected behavior.
+    ///
     /// [pidfd]: https://man7.org/linux/man-pages/man2/pidfd_open.2.html
     #[inline]
     pub const fn from_raw(raw: RawPid) -> Option<Self> {
+        debug_assert!(raw > 0);
         match NonZeroI32::new(raw) {
             Some(non_zero) => Some(Self(non_zero)),
             None => None,
@@ -46,9 +50,12 @@ impl Pid {
 
     /// Converts a known positive `RawPid` into a `Pid`.
     ///
+    /// Passing a negative number doesn't invoke undefined behavior, but it
+    /// may cause unexpected behavior.
+    ///
     /// # Safety
     ///
-    /// The caller must guarantee `raw` is positive.
+    /// The caller must guarantee `raw` is non-zero.
     #[inline]
     pub const unsafe fn from_raw_unchecked(raw: RawPid) -> Self {
         debug_assert!(raw > 0);

commit 82c08008efd4ea65210d47f8067057785477c956

Fix the assertion in `Pid::from_raw` to accept 0.
Fix a regression from #1443 which disallowed calling `Pid::from_raw`
with the value 0.

src/pid.rs
@@ -41,7 +41,7 @@ impl Pid {
     /// [pidfd]: https://man7.org/linux/man-pages/man2/pidfd_open.2.html
     #[inline]
     pub const fn from_raw(raw: RawPid) -> Option<Self> {
-        debug_assert!(raw > 0);
+        debug_assert!(raw >= 0);
         match NonZeroI32::new(raw) {
             Some(non_zero) => Some(Self(non_zero)),
             None => None,
@@ -79,6 +79,8 @@ impl Pid {
     }
 
     /// Converts a `Pid` into a `RawPid`.
+    ///
+    /// This is the same as `self.as_raw_nonzero().get()`.
     #[inline]
     pub const fn as_raw_pid(self) -> RawPid {
         self.0.get()
@@ -120,5 +122,22 @@ mod tests {
         const_assert_eq!(4567 as RawPid, unsafe {
             transmute::<Option<Pid>, RawPid>(Some(Pid::from_raw_unchecked(4567)))
         });
+    }
+
+    #[test]
+    fn test_ctors() {
+        use std::num::NonZero;
+        assert!(Pid::from_raw(0).is_none());
+        assert_eq!(
+            Pid::from_raw(77).unwrap().as_raw_nonzero(),
+            NonZero::new(77).unwrap()
+        );
+        assert_eq!(Pid::from_raw(77).unwrap().as_raw_pid(), 77);
+        assert_eq!(Pid::as_raw(Pid::from_raw(77)), 77);
+    }
+
+    #[test]
+    fn test_specials() {
+        assert!(Pid::from_raw(1).unwrap().is_init());
     }
 }