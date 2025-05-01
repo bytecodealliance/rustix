7e90ae243265b15ffae6c7df2f70772abd98eecf
rusty-snake <41237666+rusty-snake@users.noreply.github.com>
2025-05-01 02:13:35 +0200

    Rework Capability types (#1436)
    - Rename CapabilityFlags to CapabilitySet
    - Deprecated Capability, use CapabilitySet instead.
    - Add sealed CompatCapability trait.

--- tests/system/reboot.rs
+++ tests/system/reboot.rs
@@ -3,15 +3,15 @@
 fn test_reboot() {
     use rustix::io::Errno;
     use rustix::system::{self, RebootCommand};
-    use rustix::thread::{self, CapabilityFlags};
+    use rustix::thread::{self, CapabilitySet};
 
     let mut capabilities = thread::capabilities(None).expect("Failed to get capabilities");
 
-    capabilities.effective.set(CapabilityFlags::SYS_BOOT, false);
+    capabilities.effective.set(CapabilitySet::SYS_BOOT, false);
 
     thread::set_capabilities(None, capabilities).expect("Failed to set capabilities");
 
-    // The reboot syscall requires the `CapabilityFlags::SYS_BOOT` permission
+    // The reboot syscall requires the `CapabilitySet::SYS_BOOT` permission
     // to be called, otherwise [`Errno::PERM`] is returned
     assert_eq!(system::reboot(RebootCommand::Restart), Err(Errno::PERM));
 }
